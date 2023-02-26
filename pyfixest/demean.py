a7acc544fef055e387d7de50b9e260b9c0a5cf6e
Alexander Fischer <alexander-fischer1801@t-online.de>
2023-02-26 22:56:34 +0100

    fix multiple errors in demean() function

--- pyfixest/demean.py
+++ pyfixest/demean.py
@@ -2,89 +2,70 @@ import numpy as np
 from numba import jit, njit
 
 @njit
+def ave(x, f):
+
+    '''
+    helper function: compute group-wise averages
+    '''
+
+    N = x.shape[0]
+    res = np.zeros(N)
+    unique_f = np.unique(f)
+    for i, ival in enumerate(unique_f):
+        idx = np.where(np.equal(f, ival))
+        res[idx] = np.mean(x[idx])
+
+    return res
+
+@njit
 def demean(x, fixef_vars, tol = 1e-10):
 
     '''
     demean variables by MAP algorithm
-
-    Args:
+    
+    Args: 
         x (np.array): Variable(s) to demean
         fixef_vars (np.array): fixef_vars to demean by
-
-    Returns:
-        A np.array of the same dimension as x, with demeaned x.
-
-    Examples:
-
+        
+    Returns: 
+        A np.array of the same dimension as x, with demeaned x. 
+        
+    Examples: 
+        
         N = 100000
         fixef_vars = np.random.choice([0, 1, 2, 3, 4, 5, 6], N)
         fixef_vars.shape = (fixef_vars.shape[0], 1)
         x = np.random.normal(0, 1, N)
         demean(x, g)
-
-    Notes:
-
-        The algorithm follows the vignette of the lfe R package by Simen Gaure.
-        The R example code therin is
-
-        demean <- function(x, flist) {
-            cx <- x
-            oldx <- x - 1
-            while(sqrt(sum((cx - oldx) ^ 2)) >= 1e-10) {
-                oldx <- cx
-                for(i in 1:length(flist)){
-                cx <- cx - ave(cx, flist[[i]])
-                }
-            }
-            return(cx)
-        }
-
+    
     '''
+    
+    isna_fixef = np.sum(np.isnan(fixef_vars))
+    if isna_fixef > 0: 
+        raise Exception('Fixef Effects include missings, which is not allowed.')
 
+    #if len(fixef_vars.shape) == 1: 
+    #    fixef_vars = fixef_vars.reshape(len(fixef_vars), 1)
 
-    def ave(x, f):
-
-        '''
-        helper function: compute group-wise averages
-        '''
-
-        N = x.shape[0]
-        res = np.empty(N)
-        res[:] = np.nan
-        unique_f = np.unique(f[~np.isnan(f)])
-        for i, ival in enumerate(unique_f):
-            idx = np.where(np.equal(f, i))
-            res[idx] = np.mean(x[idx])
-
-        return res
-
-    if len(fixef_vars.shape) == 1:
-        fixef_vars.reshape(N, 1)
-
-    k_fixef = fixef_vars.shape[1]
-
-    #isna_fixef = np.all(np.isnan(fixef_vars), axis = 1)
-    isna_fixef = np.sum(np.isnan(fixef_vars), axis = 1)
-
+    k_fixef = 1
+    
     cx = x
-    cx[isna_fixef] = np.nan
     oldx = x - 1
 
-    while np.sqrt(np.power(np.sum(cx - oldx), 2)) >= tol:
+    while np.sqrt(np.power(np.nansum(cx - oldx), 2)) >= tol:
         oldx = cx
-        for f in range(0, k_fixef-1):
-            cx = cx - ave(x, fixef_vars[:,f])
+        for i in range(k_fixef):
+            cx = cx - ave(cx, fixef_vars[:,i])
 
     return cx
 
 
+
+
 # N = 100000
-# fixef_vars = np.random.choice([0, 1, 2, 3, 4, 5, 6], N*2).astype(float)
-# fixef_vars = fixef_vars.reshape(N, 2)
-# fixef_vars[1, 1] = np.nan
-# #fixef_vars.shape = (fixef_vars.shape[0], 1)
+# fixef_vars = np.random.choice([0, 1, 2, 3, 4, 5, 6], N)
+# fixef_vars.shape = (fixef_vars.shape[0], 1)
 # x = np.random.normal(0, 1, N)
 # x[0] = np.nan
-# res = demean(x, fixef_vars)
-# res
-# len(res)
+# demean(x, fixef_vars)[0:5]
+# x[0:5]
