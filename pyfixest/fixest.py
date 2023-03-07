137f6a4d29913270407f3787fe55efb945a80deb
Alexander Fischer <alexander-fischer1801@t-online.de>
2023-03-07 23:06:24 +0100

    more pythonic with proper objects etc

--- pyfixest/fixest.py
+++ pyfixest/fixest.py
@@ -5,30 +5,26 @@ import numpy as np
 import pandas as pd
 from scipy.stats import norm
 from formulaic import model_matrix
+from tabulate import tabulate
 
 
-from pyfixest.demean import demean
+from pyfixest.feols import Feols
 from pyfixest.FormulaParser import FixestFormulaParser, _flatten_list
 
 
 class Fixest:
 
-    def __init__(self, fml, data):
+    def __init__(self, data):
 
         '''
         Initiate the fixest object.
         Deparse fml into formula dict, variable dict.
         '''
-        fml = FixestFormulaParser(fml)
-        fml.get_fml_dict()
-        fml.get_var_dict()
-
         self.data = data
-        self.fml_dict = fml.fml_dict
-        self.var_dict = fml.var_dict
+        self.model_res = dict()
 
-
-    def demean(self):
+        
+    def _demean(self):
 
         # deparse fxst.fml_dict:
         fixef_keys = list(self.var_dict.keys())
@@ -111,159 +107,81 @@ class Fixest:
               YX_dict[list(var_dict2.keys())[x]] = data_demean
 
             self.demeaned_data_dict[fval] = YX_dict
-
-
-
-class Feols:
-
-    def __init__(self, Y, X):
-
+ 
+    def feols(self, fml, vcov):
+    
+        '''
+        fixest function for regression modeling
+        fixed effects are projected out via the PyHDFE package
+    
+        Args:
+    
+          fml (string, patsy Compatible): of the form Y ~ X1 + X2 | fe1 + fe2
+          vcov (string or dict): either 'iid', 'hetero', 'HC1', 'HC2', 'HC3'. For
+                                 cluster robust inference, a dict {'CRV1':'clustervar'}
+                                 for CRV1 inference or {'CRV3':'clustervar'} for CRV3
+                                 inference.
+    
+        Returns:
+    
+          A dictionariy with
+            - estimated regression coefficients
+            - standard errors
+            - variance-covariance matrix
+            - t-statistics
+            - p-values
+        '''
+      
+        fml = FixestFormulaParser(fml)
         
-        #coefnames = X.columns
-        #depvars = Y.columns
-
-        self.Y = np.array(Y, dtype = "float64")
-        self.X = np.array(X, dtype = "float64")
-        self.N, self.k = X.shape
-
-        # drop intercept when fixed effects
-        # are present
-        #X = X[:, coefnames != 'Intercept']
-        #self.coefnames = coefnames[coefnames != 'Intercept']
-
-    def fit(self):
-        '''
-        regression estimation for a single model
-        '''
-
-        self.tXX = np.transpose(self.X) @ self.X
-        self.tXXinv = np.linalg.inv(self.tXX)
-
-        self.tXy = (np.transpose(self.X) @ self.Y)
-        beta_hat = self.tXXinv @ self.tXy
-        self.beta_hat = beta_hat.flatten()
-        self.Y_hat = (self.X @ self.beta_hat)
-        self.u_hat = (self.Y - self.Y_hat)
-
-    def vcov(self, vcov = "hetero"):
-
-        '''
-        compute covariance matrices
-        '''
-
-        if isinstance(vcov, dict):
-            vcov_type_detail = list(vcov.keys())[0]
-            self.clustervar = list(vcov.values())[0]
-        elif isinstance(vcov, list):
-            vcov_type_detail = vcov
-        elif isinstance(vcov, str):
-            vcov_type_detail = vcov
-        else:
-            assert False, "arg vcov needs to be a dict, string or list"
-
-        if vcov_type_detail == "iid":
-            vcov_type = "iid"
-        elif vcov_type_detail in ["hetero", "HC1", "HC2", "HC3"]:
-            vcov_type = "hetero"
-        elif vcov_type_detail in ["CRV1", "CRV3"]:
-            vcov_type = "CRV"
-
-
-        # compute vcov
-        if vcov_type == 'iid':
-
-            self.vcov = (self.tXXinv * np.mean(self.u_hat ** 2))
-
-        elif vcov_type == 'hetero':
-
-            if vcov_type_detail in ["hetero", "HC1"]:
-                self.ssc = (self.N / (self.N - self.k))
-                u = self.u_hat
-            elif vcov_type_detail in ["HC2", "HC3"]:
-                self.ssc = 1
-                leverage = np.mean(self.X * (self.X @ self.tXXinv), axis=1)
-                if vcov_type_detail == "HC2":
-                     u = (1 - leverage) * self.u_hat
-                else:
-                    u = np.sqrt(1 - leverage) * self.u_hat
-
-            meat = np.transpose(self.X) * (u ** 2) @ self.X
-            self.vcov = self.ssc * self.tXXinv @ meat @  self.tXXinv
-
-        elif vcov_type == "CRV":
-
-            # if there are missings - delete them!
-            cluster_df = np.array(self.data[self.clustervar])
-            # drop NAs
-            #if len(self.na_index) != 0:
-            #    cluster_df = np.delete(cluster_df, 0, self.na_index)
-
-            clustid = np.unique(cluster_df)
-            self.G = len(clustid)
-
-            if vcov_type_detail == "CRV1":
-
-                meat = np.zeros((self.k, self.k))
-
-                for igx, g, in enumerate(clustid):
-
-                    Xg = self.X[np.where(cluster_df == g)]
-                    ug = self.u_hat[np.where(cluster_df == g)]
-                    score_g = (np.transpose(Xg) @ ug).reshape((self.k, 1))
-                    meat += np.dot(score_g, score_g.transpose())
-
-                self.ssc = self.G / (self.G - 1) * (self.N-1) / (self.N-self.k)
-                self.vcov = self.ssc * self.tXXinv @ meat @ self.tXXinv
-
-            elif vcov_type_detail == "CRV3":
-
-                # check: is fixed effect cluster fixed effect?
-                # if not, either error or turn fixefs into dummies
-                # for now: don't allow for use with fixed effects
-                assert self.has_fixef == False, "CRV3 currently not supported with arbitrary fixed effects"
-
-                beta_jack = np.zeros((self.G, self.k))
-                tXX = np.transpose(self.X) @ self.X
-
-                for ixg, g in enumerate(clustid):
-
-
-                    Xg = self.X[np.where(cluster_df == g)]
-                    Yg = self.Y[:, x][np.where(cluster_df == g)]
-
-                    tXgXg = np.transpose(Xg) @ Xg
-
-                    # jackknife regression coefficient
-                    beta_jack[ixg, :] = (
-                        np.linalg.pinv(
-                            tXX - tXgXg) @ (self.tXy - np.transpose(Xg) @ Yg)
-                    ).flatten()
-
-                beta_center = self.beta_hat
-
-                vcov = np.zeros((self.k, self.k))
-                for ixg, g in enumerate(clustid):
-                    beta_centered = beta_jack[ixg, :] - beta_center
-                    vcov += np.outer(beta_centered, beta_centered)
-
-                self.ssc = self.G / (self.G - 1)
-
-                self.vcov = self.ssc * vcov
-
-    def inference(self):
-
-        self.se = (
-            np.sqrt(np.diagonal(self.vcov))
-        )
-        self.tstat = (
-            self.beta_hat / self.se
-        )
-        self.pvalue = (
-            2*(1-norm.cdf(np.abs(self.tstat)))
-        )
-
-    def performance(self):
-
-        self.r_squared = 1 - np.sum(self.u_hat ** 2) / \
-            np.sum((self.Y - np.mean(self.Y))**2)
-        self.adj_r_squared = (self.N - 1) / (self.N - self.k) * self.r_squared
+        fml.get_fml_dict()
+        fml.get_var_dict()
+        
+        self.fml_dict = fml.fml_dict
+        self.var_dict = fml.var_dict
+        
+        self._demean()
+        
+        for f, fval in enumerate(self.fml_dict.keys()):
+            model_frames = self.demeaned_data_dict[fval]
+            for x, fml in enumerate(model_frames):
+                
+                model_frame = model_frames[fml]
+                Y = np.array(model_frame.iloc[:,0])
+                X = model_frame.iloc[:,1:]
+                colnames = X.columns
+                X = np.array(X)
+                FEOLS = Feols(Y, X)
+                FEOLS.fit()
+                FEOLS.data = self.data
+                FEOLS.vcov(vcov = vcov)
+                FEOLS.inference()
+                FEOLS.coefnames = colnames
+                full_fml = fml + "|" + fval
+                self.model_res[full_fml] = FEOLS
+            
+    def summary(self, type = None): 
+      
+        res = []
+        for x in list(self.model_res.keys()):
+            
+            fxst = self.model_res[x]
+          
+            res.append(
+                pd.DataFrame(
+                    {
+                        'fml': x,
+                        'coefnames':fxst.coefnames, 
+                        'coef': fxst.beta_hat,
+                        'se': fxst.se,
+                        'tstat': fxst.tstat,
+                        'pvalue': fxst.pvalue
+                    }
+                )
+            )
+        
+        res = pd.concat(res, axis = 0).set_index('fml')
+        if type == "markdown": 
+            return res.to_markdown(floatfmt=".3f")
+        else: 
+            return res
--- readme.md
+++ readme.md
@@ -5,64 +5,37 @@ This is a draft package (highly experimental!) for a Python clone of the excelle
 Fixed effects are projected out via the [PyHDFE](https://github.com/jeffgortmaker/pyhdfe) package.
 
 ```python
-# create some data
 import pandas as pd
 import numpy as np
-from pyfixest.api import feols
+from pyfixest.fixest import Fixest
 from pyfixest.utils import get_data
 
-# create data
-np.random.seed(123)
 data = get_data()
 
-feols('Y ~ X1 | X2 + X3 + X4', 'hetero', data)
-#   colnames      coef        se    tstat    pvalue
-# 0       X1  0.001469  0.003159  0.46505  0.641896
-feols('Y ~ X1 + X2 + X3 + X4', 'iid', data)
-#   colnames      coef        se    tstat    pvalue
-# 2048         X1    0.001469  0.003159     0.465050  6.418959e-01
-sm.ols('Y ~ X1 + X2 + X3 + X4', data).fit().summary()
-#   colnames      coef        se    tstat    pvalue
-# X1            0.0015      0.003      0.460      0.645
+fixest = Fixest(data = data)
+fixest.feols("Y~X1 | X2 + X3", vcov = "HC1")
+fixest.summary()
+#            coefnames      coef        se    tstat   pvalue
+# fml                                                       
+# Y~X1|X2+X3        X1 -0.063819  0.164427 -0.38813  0.69792
 
-# cluster robust inference:
-feols(fml = 'Y ~ X1', vcov = {'CRV1':'group_id'}, data = data)
-#     colnames        coef        se       tstat    pvalue
-# 0  Intercept -577.090042  1.072007 -538.326702  0.000000
-# 1         X1    1.389563  1.002708    1.385810  0.165805
-feols(fml = 'Y ~ X1', vcov = {'CRV3':'group_id'}, data = data)
-#     colnames        coef        se       tstat    pvalue
-# 0  Intercept -577.090042  1.139483 -506.449086  0.000000
-# 1         X1    1.389563  1.066219    1.303261  0.192486
-```
+fixest.feols("Y~X1  | X2 + X3 + X4", vcov = "HC1")
+fixest.summary()
+#               coefnames      coef        se     tstat    pvalue
+# fml                                                            
+# Y~X1|X2+X3           X1 -0.063819  0.164427 -0.388130  0.697920
+# Y~X1|X2+X3+X4        X1 -0.010369  0.010073 -1.029399  0.303292
 
-## Multiple Estimations
-
-Currently supported: multiple dependent variables, `sw()`, `sw0()`, `csw()` and `csw0()`.
-
-```python
-# sw
-# multiple estimation: multiple dependent variables
-data.X2 = data.X2.astype(float)
-feols(fml = 'Y + Y2 ~ sw(X1, X2) | csw0(X3, X4)', vcov = {'CRV1':'group_id'}, data = data)
-# fml	       coefnames	coef	se	tstat	pvalue
-# 0	Y~X1|0	   Intercept	7.386158	0.162152	45.550697	0.000000
-# 1	Y~X1|0	         X1	-0.163744	0.166210	-0.985159	0.324546
-# 0	Y~X2|0	   Intercept	20.069156	0.300012	66.894580	0.000000
-# 1	Y~X2|0	         X2	-0.516267	0.009976	-51.751797	0.000000
-# 0	Y2~X1|0	   Intercept	7.375426	0.162093	45.501283	0.000000
-# 1	Y2~X1|0	         X1	-0.179765	0.164399	-1.093465	0.274190
-# 0	Y2~X2|0	   Intercept	20.065154	0.302652	66.297875	0.000000
-# 1	Y2~X2|0	         X2	-0.516542	0.010137	-50.956222	0.000000
-# 0	Y~X1|X3          X1	-0.117885	0.154998	-0.760557	0.446922
-# 0	Y~X2|X3	         X2	-0.515981	0.009493	-54.354837	0.000000
-# 0	Y2~X1|X3	       X1	-0.134384	0.153802	-0.873746	0.382257
-# 0	Y2~X2|X3	       X2	-0.516327	0.009654	-53.485357	0.000000
-# 0	Y~X1|X3+X4	     X1	-0.063646	0.082916	-0.767600	0.442725
-# 0	Y~X2|X3+X4	     X2	-0.509159	0.000583	-872.642351	0.000000
-# 0	Y2~X1|X3+X4	     X1	-0.079504	0.082208	-0.967109	0.333489
-# 0	Y2~X2|X3+X4	     X2	-0.509534	0.001034	-492.767382	0.000000
-
+fixest.feols("Y~X1 | csw0(X2, X3)", vcov = "HC1")
+fixest.summary()
+#                coefnames      coef        se      tstat    pvalue
+# fml                                                              
+# Y~X1|X2+X3            X1 -0.063819  0.164427  -0.388130  0.697920
+# Y~X1|X2+X3+X4         X1 -0.010369  0.010073  -1.029399  0.303292
+# Y~X1|0         Intercept  7.386158  0.187834  39.322750  0.000000
+# Y~X1|0                X1 -0.163744  0.186504  -0.877964  0.379963
+# Y~X1|X2               X1 -0.103285  0.172965  -0.597142  0.550412
+ 
 
 ```
 
