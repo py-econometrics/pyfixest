11674fa0edbacc2215ae7a86ac0191b7a8eddf9d
Alexander Fischer <alexander-fischer1801@t-online.de>
2023-04-08 22:54:06 +0200

    update pvalue tests

--- tests/test_ses.py
+++ tests/test_ses.py
@@ -21,11 +21,12 @@ def test_HC1_vs_CRV1(data):
     fixest.feols('Y ~ X1', vcov = {'CRV1':'id'}, ssc = ssc(cluster_adj = False))
     res_crv1 = fixest.tidy()
 
-    if not np.allclose(res_hc1["Std. Error"], res_crv1["Std. Error"], rtol = 1e-12):
+    if not np.allclose(res_hc1["Std. Error"], res_crv1["Std. Error"]):
         raise ValueError("HC1 and CRV1 ses are not the same.")
 
-    if not np.allclose(res_hc1["t value"], res_crv1["t value"], rtol = 1e-12):
+    if not np.allclose(res_hc1["t value"], res_crv1["t value"]):
         raise ValueError("HC1 and CRV1 t values are not the same.")
 
-    if not np.allclose(res_hc1["Pr(>|t|)"], res_crv1["Pr(>|t|)"], rtol = 1e-12):
+    # should be differen, as normal and t(G-1)
+    if np.allclose(res_hc1["Pr(>|t|)"], res_crv1["Pr(>|t|)"]):
         raise ValueError("HC1 and CRV1 p values are not the same.")
