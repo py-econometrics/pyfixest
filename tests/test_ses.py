eeead534034ecc94363ce367dc449bbd51f5fe8c
Alexander Fischer <alexander-fischer1801@t-online.de>
2023-04-16 16:01:27 +0200

    test hc1 vs crv1, hc3 vs crv3

--- tests/test_ses.py
+++ tests/test_ses.py
@@ -12,40 +12,42 @@ def data():
     return data
 
 
-@pytest.mark.skip("currently not possible to test for equality due to different ssc factors")
 def test_HC1_vs_CRV1(data):
 
     fixest = pf.Fixest(data = data)
-    fixest.feols('Y ~ X1', vcov = "HC1")
+    fixest.feols('Y~X1', vcov = "HC1", ssc = ssc(adj = False, cluster_adj = False))
     res_hc1 = fixest.tidy()
 
-    fixest.feols('Y ~ X1', vcov = {'CRV1':'id'}, ssc = ssc(adj = False, cluster_adj = False))
+    fixest.feols('Y~X1', vcov = {'CRV1':'id'}, ssc = ssc(adj = False, cluster_adj = False))
     res_crv1 = fixest.tidy()
 
+    _, B = next(iter(fixest.model_res.items()))
+    N = B.N
 
-
-    if not np.allclose(res_hc1["Std. Error"], res_crv1["Std. Error"]):
+    if not np.allclose(res_hc1["Std. Error"] * np.sqrt((N-1)/ N), res_crv1["Std. Error"]):
         raise ValueError("HC1 and CRV1 ses are not the same.")
 
-    if not np.allclose(res_hc1["t value"], res_crv1["t value"]):
+    if not np.allclose(res_hc1["t value"] / np.sqrt((N-1)/ N), res_crv1["t value"]):
         raise ValueError("HC1 and CRV1 t values are not the same.")
 
-    if not np.allclose(res_hc1["Pr(>|t|)"], res_crv1["Pr(>|t|)"]):
-        raise ValueError("HC1 and CRV1 p values are not the same.")
+    #if not np.allclose(res_hc1["Pr(>|t|)"], res_crv1["Pr(>|t|)"]):
+    #    raise ValueError("HC1 and CRV1 p values are not the same.")
 
-@pytest.mark.skip("currently not possible to test for equality due to different ssc factors")
 def test_HC3_vs_CRV3(data):
 
     fixest = pf.Fixest(data = data)
-    fixest.feols('Y ~ X1', vcov = "HC3")
+    fixest.feols('Y~X1', vcov = "HC3", ssc = ssc(adj = False, cluster_adj = False))
     res_hc3 = fixest.tidy()
 
-    fixest.feols('Y ~ X1', vcov = {'CRV3':'id'}, ssc = ssc(adj = False, cluster_adj = False))
+    fixest.feols('Y~X1', vcov = {'CRV3':'id'}, ssc = ssc(adj = False, cluster_adj = False))
     res_crv3 = fixest.tidy()
 
-    if not np.allclose(res_hc3["Std. Error"], res_crv3["Std. Error"]):
+    _, B = next(iter(fixest.model_res.items()))
+    N = B.N
+
+    if not np.allclose(res_hc3["Std. Error"] * np.sqrt((N-1)/ N), res_crv3["Std. Error"]):
         raise ValueError("HC3 and CRV3 ses are not the same.")
-    if not np.allclose(res_hc3["t value"], res_crv3["t value"]):
+    if not np.allclose(res_hc3["t value"] / np.sqrt((N-1)/ N), res_crv3["t value"]):
         raise ValueError("HC3 and CRV3 t values are not the same.")
-    if not np.allclose(res_hc3["Pr(>|t|)"], res_crv3["Pr(>|t|)"]):
-        raise ValueError("HC3 and CRV3 p values are not the same.")
+    #if not np.allclose(res_hc3["Pr(>|t|)"], res_crv3["Pr(>|t|)"]):
+    #    raise ValueError("HC3 and CRV3 p values are not the same.")
