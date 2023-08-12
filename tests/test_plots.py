a083c3a091a1aecd318902e78acc11ba8bcc592e
Alexander Fischer <alexander-fischer1801@t-online.de>
2023-08-12 17:34:31 +0200

    reorganize tests

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -10,24 +10,24 @@ matplotlib.use("Agg")
 @pytest.fixture
 def data():
     data = get_data()
-    data["X2"] = pd.Categorical(data["X2"])
+    data["f2"] = pd.Categorical(data["f2"])
     return data
 
 
 def test_iplot(data):
     fixest = Fixest(data)
-    fixest.feols("Y ~ i(X2, X1) | X3", vcov="iid").iplot()
-    fixest.feols("Y ~ i(X2, X1, ref = 1)", vcov="iid").iplot()
+    fixest.feols("Y ~ i(f2, X1) | f1", vcov="iid").iplot()
+    fixest.feols("Y ~ i(f2, X1, ref = 1)", vcov="iid").iplot()
     with pytest.raises(ValueError):
         fixest.feols("Y ~ X1", vcov="iid").iplot()
 
 
 def test_coefplot(data):
     fixest = Fixest(data)
-    fixest.feols("Y ~ i(X2, X1) | X3", vcov="iid").coefplot()
+    fixest.feols("Y ~ i(f2, X1) | f1", vcov="iid").coefplot()
 
     fixest2 = Fixest(data)
-    fixest2.feols("Y ~ i(X2, X1) | X3", vcov="iid").coefplot()
+    fixest2.feols("Y ~ i(f2, X1) | f1", vcov="iid").coefplot()
 
     fixest3 = Fixest(data)
-    fixest3.feols("Y ~ X1 + X4", vcov="iid").coefplot()
+    fixest3.feols("Y ~ X1 + X2", vcov="iid").coefplot()
