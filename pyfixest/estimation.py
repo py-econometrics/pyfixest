7f293c589123b1db10bb25f6564dd3deedf67a5a
Alexander Fischer <alexander-fischer1801@t-online.de>
2023-09-06 21:45:17 +0200

    move iwls params from Fixest to fepois

--- pyfixest/estimation.py
+++ pyfixest/estimation.py
@@ -123,17 +123,79 @@ def fepois(
     vcov: Optional[Union[str, Dict[str, str]]] = None,
     ssc=ssc(),
     fixef_rm: str = "none",
+    iwls_tol: float = 1e-08,
+    iwls_maxiter: int = 25,
 ) -> None:
     """
-    Method for Estimation of Poisson Regression with high-dimensional fixed effects. See `feols()` for more details.
+    Method for fixed effects regression modeling using the PyHDFE package for projecting out fixed effects.
     Args:
+        fml (str): A three-sided formula string using fixest formula syntax. Supported syntax includes:
+            The syntax is as follows: "Y ~ X1 + X2 | FE1 + FE2 | X1 ~ Z1" where:
+
+            Y: Dependent variable
+            X1, X2: Independent variables
+            FE1, FE2: Fixed effects
+            Z1, Z2: Instruments
+            |: Separates left-hand side, fixed effects, and instruments
+
+            If no fixed effects and instruments are specified, the formula can be simplified to "Y ~ X1 + X2".
+            If no instruments are specified, the formula can be simplified to "Y ~ X1 + X2 | FE1 + FE2".
+            If no fixed effects are specified but instruments are specified, the formula can be simplified to "Y ~ X1 + X2 | X1 ~ Z1".
+
+            Supported multiple estimation syntax includes:
+
+            Stepwise regressions (sw, sw0)
+            Cumulative stepwise regression (csw, csw0)
+            Multiple dependent variables (Y1 + Y2 ~ X)
+
+            Other special syntax includes:
+            i() for interaction of a categorical and non-categorical variable (e.g. "i(X1,X2)" for interaction between X1 and X2).
+            Using i() is required to use with some custom methods, e.g. iplot().
+            ^ for interacted fixed effects (e.g. "fe1^fe2" for interaction between fe1 and fe2)
+
+            All other parts of the formula must be compatible with formula parsing via the formulaic module.
+            You can use formulaic functionaloty such as "C", "I", ":",, "*", "np.log", "np.power", etc.
+
+        vcov (Union(str, dict)): A string or dictionary specifying the type of variance-covariance matrix to use for inference.
+            If a string, it can be one of "iid", "hetero", "HC1", "HC2", "HC3".
+            If a dictionary, it should have the format dict("CRV1":"clustervar") for CRV1 inference or dict(CRV3":"clustervar") for CRV3 inference.
+        fixef_rm: A string specifiny whether singleton fixed effects should be dropped. Options are "none" (default) and "singleton". If "singleton", singleton fixed effects are dropped.
+        iwls_tol: tolerance for IWLS convergence. 1e-08 by default.
+        iwls_maxiter: maximum number of iterations for IWLS convergence. 25 by default.
 
     Returns:
-        An instance of the Fepois class.
+        An instance of the Feols class.
+    Examples:
+        Standard formula:
+            fml = 'Y ~ X1 + X2'
+            fixest_model = Fixest(data=data).feols(fml, vcov='iid')
+        With fixed effects:
+            fml = 'Y ~ X1 + X2 | fe1 + fe2'
+        With interacted fixed effects:
+            fml = 'Y ~ X1 + X2 | fe1^fe2'
+        Multiple dependent variables:
+            fml = 'Y1 + Y2 ~ X1 + X2'
+        Stepwise regressions (sw and sw0):
+            fml = 'Y1 + Y2 ~ sw(X1, X2, X3)'
+        Cumulative stepwise regressions (csw and csw0):
+            fml = 'Y1 + Y2 ~ csw(X1, X2, X3) '
+        Combinations:
+            fml = 'Y1 + Y2 ~ csw(X1, X2, X3) | sw(X4, X5) + X6'
+        With instruments:
+            fml = 'Y ~ X1 + X2 | X1 ~ Z1'
+        With instruments and fixed effects:
+            fml = 'Y ~ X1 + X2 | X1 ~ Z1  | fe1 + fe2'
+
+    Returns:
+        An instance of a Fixest class.
+
+
 
     """
 
     fixest = Fixest(data=data)
+    fixest._iwls_maxiter = iwls_maxiter
+    fixest._iwls_tol = iwls_tol
 
     fixest._prepare_estimation(
         estimation="fepois", fml=fml, vcov=vcov, ssc=ssc, fixef_rm=fixef_rm
--- tests/test_wildboottest.py
+++ tests/test_wildboottest.py
@@ -10,7 +10,7 @@ def data():
 
 # note - tests currently fail because of ssc adjustments
 def test_hc_equivalence(data):
-    fixest = feols(fml = "Y~csw(X1, X2, f1)", data = data)
+    fixest = feols(fml="Y~csw(X1, X2, f1)", data=data)
     tstat = fixest.tstat().reset_index().set_index("Coefficient").xs("X1")
     boot_tstat = fixest.wildboottest(param="X1", B=999)["t value"]
 
@@ -18,8 +18,7 @@ def test_hc_equivalence(data):
 
 
 def test_crv1_equivalence(data):
-
-    fixest = feols(fml = "Y~csw(X1, X2, f1)", data = data, vcov={"CRV1": "group_id"})
+    fixest = feols(fml="Y~csw(X1, X2, f1)", data=data, vcov={"CRV1": "group_id"})
     tstat = fixest.tstat().reset_index().set_index("Coefficient").xs("X1")
     boot_tstat = fixest.wildboottest(param="X1", B=999)["t value"]
 
