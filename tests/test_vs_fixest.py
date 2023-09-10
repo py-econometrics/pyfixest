5b622edab691217e930f780c8eca895aafdcc367
Alexander Fischer <alexander-fischer1801@t-online.de>
2023-09-10 16:05:06 +0200

    attempt to fix CI error

--- tests/test_vs_fixest.py
+++ tests/test_vs_fixest.py
@@ -30,6 +30,7 @@ iwls_tol = 1e-08
 
 rng = np.random.default_rng(8760985)
 
+
 @pytest.mark.parametrize("N", [1000])
 @pytest.mark.parametrize("seed", [76540251])
 @pytest.mark.parametrize("beta_type", ["1", "2", "3"])
