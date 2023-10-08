51c15d4f908e152e1f5fa1dd6ad7b76e9611f075
Alexander Fischer <alexander-fischer1801@t-online.de>
2023-10-08 13:47:01 +0200

    add some tests, but eventually, demeaning is test via OLS tests

---
+++ tests/test_custom_demean.py
@@ -0,0 +1,30 @@
+import pytest
+import numpy as np
+import pyhdfe
+from pyfixest.demean import demean
+
+
+def test_demean():
+    rng = np.random.default_rng(929291)
+
+    N = 10_000
+    x = rng.normal(0, 1, 100*N).reshape((N,100))
+    f1 = rng.choice(list(range(100)), N).reshape((N,1))
+    f2 = rng.choice(list(range(100)), N).reshape((N,1))
+
+    flist = np.concatenate((f1, f2), axis = 1)
+
+    # without weights
+    weights = np.ones(N)
+    algorithm = pyhdfe.create(flist)
+    res_pyhdfe = algorithm.residualize(x)
+    res_pyfixest, success = demean(x, flist, weights, tol = 1e-10)
+    assert np.allclose(res_pyhdfe , res_pyfixest)
+
+    # with weights
+    weights = rng.uniform(0, 1, N).reshape((N,1))
+    algorithm = pyhdfe.create(flist)
+    res_pyhdfe = algorithm.residualize(x, weights)
+    res_pyfixest, success = demean(x, flist, weights.flatten(), tol = 1e-10)
+    assert np.allclose(res_pyhdfe , res_pyfixest)
+
