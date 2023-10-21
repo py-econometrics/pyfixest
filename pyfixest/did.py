fa7dfc3c113fadb608a1d5f95dfd8e08fac7282d
Alexander Fischer <alexander-fischer1801@t-online.de>
2023-10-21 22:12:31 +0200

    reproduce did2s.r results

--- pyfixest/did.py
+++ pyfixest/did.py
@@ -14,13 +14,18 @@ def event_study(data, yname, idname, tname, gname, xfml, estimator):
 
         did2s = DID2S(data = data, yname = yname, idname=idname, tname = tname, gname = gname, xfml = xfml)
         fit = did2s.estimate()
-        #vcov = did2s.vcov()
-        #fit._vcov = vcov
-        # update inference with new vcov matrix
-        #fit._get_inference()
+        vcov = did2s.vcov()
+        fit._vcov = vcov
+        fit._vcov_type = "CRV1"
+        fit._vcov_type_detail = "CRV (GMM)"
+        fit._G = did2s._G
+        fit._method = "did2s"
 
     else:
         raise Exception("Estimator not supported")
+
+    # update inference with vcov matrix
+    fit.get_inference()
 
     return fit
 
@@ -78,10 +83,10 @@ class DID2S(DID):
 
         if self._xfml is not None:
             self._fml1 = f"{yname} ~ {xfml} | {idname} + {tname}"
-            self._fml2 = f"Y_hat ~ {xfml} + treat"
+            self._fml2 = f"{yname} ~ {xfml} + treat"
         else:
             self._fml1 = f"{yname} ~ 0 | {idname} + {tname}"
-            self._fml2 = f"Y_hat ~ treat"
+            self._fml2 = f"{yname} ~ - 1 + treat"
 
         #self._not_yet_treated_idx = data[data["treat"] == False] #data[gname] <= data[tname]
 
@@ -92,6 +97,7 @@ class DID2S(DID):
         _fml2 = self._fml2
         _data = self._data
         _not_yet_treated_data = _data[_data["treat"] == False]
+        _yname = self._yname
 
 
         tic = time.time()
@@ -109,52 +115,71 @@ class DID2S(DID):
         toc = time.time() - tic
         print(f"Time for predict: {toc} seconds")
 
-        _data["Y_hat"] = Y_hat
+        self._first_u = _data[f"{_yname}"].to_numpy().flatten() - Y_hat
+        _data[f"{_yname}"] = self._first_u
 
         tic = time.time()
         fit2 = feols(_fml2, data = _data)
         toc = time.time() - tic
         print(f"Time for second fit: {toc} seconds")
+        self._second_u = fit2.resid()
 
         return fit2
 
     def vcov(self, cluster = None):
 
+
         _data = self._data
+        _first_u = self._first_u
+        _second_u = self._second_u
+
+
         if cluster is None:
             cluster = self._idname
 
         cluster_col =  _data[cluster]
         _, clustid = pd.factorize(cluster_col)
 
-        fml_group_time = f"~C({self._idname}) + C({self._tname})"
-        fml_treatment_vars = "~treat"
+        self._G = clustid.nunique()
+
+        fml_group_time = f"~C({self._idname}) + C({self._tname})"   # add covariates
+        fml_treatment_vars = "~0+treat"                               # add covariates
 
         X1 = model_matrix(fml_group_time, _data, output = "sparse")
         #X10 = X1[self._not_yet_treated_idx, :]
         X2 = model_matrix(fml_treatment_vars, _data, output = "sparse")
 
-        X2X2 = (X2.T.dot(X2)).toarray()
-        X2X2inv = np.linalg.inv(X2X2)
+        X10 = X1.copy().tocsr()
+        treated_rows = np.where(_data["treat"], 0, 1)
+        X10 = X10.multiply(treated_rows[:, None])
 
-        # clustering
+        X10X10 = X10.T.dot(X10)
+        X2X1 = X2.T.dot(X1)
+        X2X2 = X2.T.dot(X2)
 
-        sum_WgWg = np.zeros((X1.shape[1], X1.shape[1]))
+        V = spsolve(X10X10, X2X1.T).T
+
+        k = X2.shape[1]
+        vcov = np.zeros((k, k))
+
+        X10 = X10.tocsr()
+        X2 = X2.tocsr()
+
         for (_,g,) in enumerate(clustid):
 
-            X1g = X1[cluster_col == g, :]
+            X10g = X10[cluster_col == g, :]
             X2g = X2[cluster_col == g, :]
-            e1g = None
-            e2g = None
+            first_u_g = _first_u[cluster_col == g]
+            second_u_g = _second_u[cluster_col == g]
 
-            A = X2g.T @ e2g
-            B = e1g.T @ X1g
-            C = spsolve(X1g.T.dot(X1g))
-            D = X1g.T.dot(X2g)
+            W_g = X2g.T.dot(second_u_g) - V @ X10g.T.dot(first_u_g)
 
-            sum_WgWg += A - B @ C @ D
+            score = spsolve(X2X2, W_g)
+            cov_g = score.dot(score.T)
 
-        self._vcov = X2X2inv @ sum_WgWg @ X2X2inv
+            vcov += cov_g
+
+        self._vcov = vcov
 
         return self._vcov
 
