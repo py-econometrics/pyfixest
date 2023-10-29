c8002a0837168d0d24375a52c713ebb752d83257
Alexander Fischer <alexander-fischer1801@t-online.de>
2023-10-29 12:50:46 +0100

    proper rendering of md and tex output

--- pyfixest/summarize.py
+++ pyfixest/summarize.py
@@ -8,13 +8,13 @@ import pandas as pd
 from typing import Union, List, Optional
 
 
-def etable(models: Union[Feols, Fepois, Feiv, List], digits: Optional[int] = 3, type: Optional[str] = "df") -> Union[pd.DataFrame, str]:
+def etable(models: Union[Feols, Fepois, Feiv, List], digits: Optional[int] = 3, type: Optional[str] = "md") -> Union[pd.DataFrame, str]:
     """
     Create an esttab-like table from a list of models#
     Args:
         models: A list of models of type Feols, Feiv, Fepois.
         digits: Number of digits to round to.
-        type: Type of output. Either "df" for pandas DataFrame, "md" for markdown, or "tex" for LaTeX table.
+        type: Type of output. Either "df" for pandas DataFrame, "md" for markdown, or "tex" for LaTeX table. "md" by default.
     Returns:
         A pandas DataFrame with the coefficients and standard errors of the models.
     """
@@ -29,20 +29,22 @@ def etable(models: Union[Feols, Fepois, Feiv, List], digits: Optional[int] = 3, 
     for i, model in enumerate(models):
 
         model = model.tidy().reset_index().round(digits)
-        model['Estimate (Std. Error)'] = model.apply(lambda row: f"{row['Estimate']} ({row['Std. Error']})", axis=1)
+        model['Estimate (Std. Error)'] = pd.Categorical(model.apply(lambda row: f"{row['Estimate']} ({row['Std. Error']})", axis=1))
         model = model[["Coefficient","Estimate (Std. Error)"]]
         model = pd.melt(model, id_vars=["Coefficient"], var_name="Metric", value_name=f"est{i+1}")
         model = model.drop("Metric", axis = 1).set_index("Coefficient")
         etable_list.append(model)
 
     res = pd.concat(etable_list, axis = 1).fillna("")
+    #separator_row = pd.DataFrame(['_' * 15, '_' * 15, '_' * 15], columns=res.columns)
+    #res = pd.concat([res, separator_row], ignore_index=True)
 
     if type == "tex":
-        res = res.to_latex(escape = True, column_format = "l" + "c" * len(models))
+        return print(res.to_latex())
     elif type == "md":
-        res = res.to_markdown()
-
-    return res
+        return print(res.to_markdown())
+    else:
+        return res
 
 def summary(
     models: Union[Feols, Fepois, Feiv, List], digits: Optional[int] = 3
