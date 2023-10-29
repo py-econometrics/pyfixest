0c3b1626897c515cb1efa80797f477854c1f739c
Alexander Fischer <alexander-fischer1801@t-online.de>
2023-10-29 13:50:06 +0100

    PyFixest 0.10.6 (#201)
    * adds `etable()`

--- pyfixest/summarize.py
+++ pyfixest/summarize.py
@@ -1,4 +1,3 @@
-from tkinter import N
 from pyfixest.feols import Feols
 from pyfixest.fepois import Fepois
 from pyfixest.feiv import Feiv
@@ -7,6 +6,47 @@ import numpy as np
 import pandas as pd
 from typing import Union, List, Optional
 
+
+def etable(models: Union[Feols, Fepois, Feiv, List], digits: Optional[int] = 3, type: Optional[str] = "md") -> Union[pd.DataFrame, str]:
+    """
+    Create an esttab-like table from a list of models#
+    Args:
+        models: A list of models of type Feols, Feiv, Fepois.
+        digits: Number of digits to round to.
+        type: Type of output. Either "df" for pandas DataFrame, "md" for markdown, or "tex" for LaTeX table. "md" by default.
+    Returns:
+        A pandas DataFrame with the coefficients and standard errors of the models.
+    """
+
+
+    models = _post_processing_input_checks(models)
+
+    assert digits >= 0, "digits must be a positive integer"
+    assert type in ["df", "tex", "md"], "type must be either 'df', 'md' or 'tex'"
+
+    etable_list = []
+    for i, model in enumerate(models):
+
+        model = model.tidy().reset_index().round(digits)
+        model["stars"] = np.where(model["Pr(>|t|)"] < 0.001, "***", np.where(model["Pr(>|t|)"] < 0.01, "**", np.where(model["Pr(>|t|)"] < 0.05, "*", "")))
+        model['Estimate (Std. Error)'] = pd.Categorical(model.apply(lambda row: f"{row['Estimate']}{row['stars']} ({row['Std. Error']})", axis=1))
+        model = model[["Coefficient","Estimate (Std. Error)"]]
+        model = pd.melt(model, id_vars=["Coefficient"], var_name="Metric", value_name=f"est{i+1}")
+        model = model.drop("Metric", axis = 1).set_index("Coefficient")
+        etable_list.append(model)
+
+    res = pd.concat(etable_list, axis = 1).fillna("")
+    #separator_row = pd.DataFrame(['_' * 15, '_' * 15, '_' * 15], columns=res.columns)
+    #res = pd.concat([res, separator_row], ignore_index=True)
+
+    if type == "tex":
+        return res.to_latex()
+    elif type == "md":
+        res = res.to_markdown()
+        print(res)
+        print("Significance levels: * p < 0.05, ** p < 0.01, *** p < 0.001")
+    else:
+        return res
 
 def summary(
     models: Union[Feols, Fepois, Feiv, List], digits: Optional[int] = 3
