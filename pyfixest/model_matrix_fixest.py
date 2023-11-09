3601f98803c69cc24fc99464864ac6b37f384963
Alexander Fischer <alexander-fischer1801@t-online.de>
2023-11-09 23:55:13 +0100

    update figure + fix small bug

--- pyfixest/model_matrix_fixest.py
+++ pyfixest/model_matrix_fixest.py
@@ -79,10 +79,11 @@ def model_matrix_fixest(
 
     _ivars = _find_ivars(fml)[0]
 
-    if len(_ivars) == 2:
-        warnings.warn(
-            "The use of two interaction variables via i(var1, var2) is currently experimental. Please report any issues via github."
-        )
+    if _ivars:
+        if len(_ivars) == 2:
+            warnings.warn(
+                "The use of two interaction variables via i(var1, var2) is currently experimental. Please report any issues via github."
+            )
 
     # step 1: deparse formula
     fml_parts = fml.split("|")
