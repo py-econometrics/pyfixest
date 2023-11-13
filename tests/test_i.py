f0c4c06ee24ce1ce5f328658fb458be3bf2e5475
Alexander Fischer <alexander-fischer1801@t-online.de>
2023-11-13 21:00:47 +0100

    fix i(var1, var2) interaction - unit tests pass

--- tests/test_i.py
+++ tests/test_i.py
@@ -88,6 +88,8 @@ def test_i():
 
 
 def test_i_vs_fixest():
+
+
     df_het = pd.read_csv("pyfixest/experimental/data/df_het.csv")
 
     # ---------------------------------------------------------------------------------------#
@@ -159,3 +161,60 @@ def test_i_vs_fixest():
     np.testing.assert_allclose(
         fit_py.coef().values, np.array(fit_r.rx2("coefficients"))
     )
+
+
+
+def test_i_interacted_fixest():
+
+
+    df_het = pd.read_csv("pyfixest/experimental/data/df_het.csv")
+    df_het["X"] = np.random.normal(df_het.shape[0])
+
+    # ---------------------------------------------------------------------------------------#
+    # no fixed effects
+
+    # no references
+    fit_py = feols("dep_var~i(state, year)", df_het)
+    fit_r = fixest.feols(ro.Formula("dep_var~i(state, year)"), df_het)
+    np.testing.assert_allclose(
+        fit_py.coef().values, np.array(fit_r.rx2("coefficients"))
+    )
+
+    if True:
+        # no reference one fixed effect
+        fit_py = feols("dep_var~i(state, year) | state ", df_het)
+        fit_r = fixest.feols(ro.Formula("dep_var~i(state, year) | state"), df_het)
+        np.testing.assert_allclose(
+            fit_py.coef().values, np.array(fit_r.rx2("coefficients"))
+        )
+
+    if True:
+        # one reference
+        fit_py = feols("dep_var~i(state, year)  ", df_het, i_ref1 = 1)
+        fit_r = fixest.feols(ro.Formula("dep_var~i(state, year, ref = 1)"), df_het)
+        np.testing.assert_allclose(
+            fit_py.coef().values, np.array(fit_r.rx2("coefficients"))
+        )
+
+    if True:
+        # one reference and fixed effect
+        fit_py = feols("dep_var~i(state, year) | state ", df_het, i_ref1 = 1)
+        fit_r = fixest.feols(ro.Formula("dep_var~i(state, year, ref = 1) | state"), df_het)
+        np.testing.assert_allclose(
+            fit_py.coef().values, np.array(fit_r.rx2("coefficients"))
+        )
+
+    if True:
+        # two references and no fixed effect
+        fit_py = feols("dep_var~i(state, year)  ", df_het, i_ref1 = [1,2])
+        fit_r = fixest.feols(ro.Formula("dep_var~i(state, year, ref = c(1,2))"), df_het)
+        np.testing.assert_allclose(
+            fit_py.coef().values, np.array(fit_r.rx2("coefficients"))
+        )
+
+        # two references and one fixed effect
+        fit_py = feols("dep_var~i(state, year) | state ", df_het, i_ref1 = [1,2])
+        fit_r = fixest.feols(ro.Formula("dep_var~i(state, year, ref = c(1,2)) | state"), df_het)
+        np.testing.assert_allclose(
+            fit_py.coef().values, np.array(fit_r.rx2("coefficients"))
+        )
