35f3c3239fa39ed491a949b82bb45a0287243f09
Alexander Fischer <alexander-fischer1801@t-online.de>
2023-11-23 19:02:13 +0100

    add CI tests for PRs, fix type error bug (#222)

--- pyfixest/feols.py
+++ pyfixest/feols.py
@@ -1280,7 +1280,7 @@ def _find_collinear_variables(X, tol=1e-10):
 
 # CODE from Styfen Schaer (@styfenschaer)
 @nb.njit(parallel=False)
-def bucket_argsort(arr: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+def bucket_argsort(arr: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     counts = np.zeros(arr.max() + 1, dtype=np.uint32)
     for i in range(arr.size):
         counts[arr[i]] += 1
