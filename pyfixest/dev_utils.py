e0fe357495de7475b7691f833cf5a4ef79c18579
Alexander Fischer <alexander-fischer1801@t-online.de>
2023-12-29 20:14:25 +0100

    mild formatting

--- pyfixest/dev_utils.py
+++ pyfixest/dev_utils.py
@@ -10,16 +10,12 @@ except ImportError:
 
 
 def _polars_to_pandas(data: DataFrameType) -> pd.DataFrame:
+
     if not isinstance(data, pd.DataFrame):
         try:
-            # Try to import Polars
             import polars as pl
-
-            # Convert user_input to a Polars DataFrame
             data = data.to_pandas()
-
         except ImportError:
-            # Polars is not available
             raise ImportError(
                 "Polars is not installed. Please install Polars to use it as an alternative."
             )
