1525743bc6e0c539d0245eb69e93049cb9dd0fe7
Alexander Fischer <alexander-fischer1801@t-online.de>
2023-12-30 16:29:24 +0100

    Overdetermined iv bug #236 (#242)
    * fix #236 #242 
    * install r packages for CI via [r2u](https://github.com/eddelbuettel/r2u)
    * require `formulaic < 1.0.0`
    * drop `pyhdfe` dependency
    * reorganise `pyproject.toml`

--- tests/test_others.py
+++ tests/test_others.py
@@ -1,6 +1,20 @@
 from pyfixest.estimation import feols, fepois
-from pyfixest.utils import get_data
+from pyfixest.utils import get_data, ssc
 import polars as pl
+import numpy as np
+
+
+def test_multicol_overdetermined_iv():
+    data = get_data()
+    fit = feols("Y ~ X2 +  f1| f1 | X1 ~ Z1 + Z2", data=data, ssc=ssc(adj=False))
+
+    assert fit._collin_vars == ["f1"]
+    assert fit._collin_vars_z == ["f1"]
+
+    np.testing.assert_allclose(
+        fit._beta_hat, np.array([-0.993607, -0.174227], dtype=float), rtol=1e-5
+    )
+    np.testing.assert_allclose(fit._se, np.array([0.104009, 0.018416]), rtol=1e-5)
 
 
 def test_polars_input():
