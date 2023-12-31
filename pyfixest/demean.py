08aedbede74bf4ba2b0de15438b1e2787e4c920a
styfenschaer <79762922+styfenschaer@users.noreply.github.com>
2023-12-31 13:09:53 +0100

    Enhance detect singletons (#231)
    * fix bug and improve performance of _detect_singletons (issue 229)
    
    * add test cases for _detect_singletons (issue 229)

--- pyfixest/demean.py
+++ pyfixest/demean.py
@@ -1,8 +1,9 @@
+from typing import Any, Dict, Optional, Sequence, Tuple
+
+import numba as nb
 import numpy as np
 import pandas as pd
-from typing import Any, Dict, Optional, Tuple
-import numba as nb
-from typing import Any, Sequence, Optional
+from numba.extending import overload
 
 
 def demean_model(
@@ -224,47 +225,106 @@ def demean(
     return (res, success)
 
 
-# @nb.jit(nopython=False)
+def _prepare_fixed_effects(ary):
+    pass
+
+
+@overload(_prepare_fixed_effects)
+def _ol_preproc_fixed_effects(ary):
+    # If array is already an F-array we tolerate
+    # any dtype because it saves us a copy
+    if ary.layout == "F":
+        return lambda ary: ary
+
+    if not isinstance(ary.dtype, nb.types.Integer):
+        raise nb.TypingError("Fixed effects must be integers")
+
+    max_nbits = 32
+    nbits = min(max_nbits, ary.dtype.bitwidth)
+    dtype = nb.types.Integer.from_bitwidth(nbits, signed=False)
+
+    def impl(ary):
+        n, m = ary.shape
+        out = np.empty((m, n), dtype=dtype).T
+        out[:] = ary[:]
+        return out
+
+    return impl
+
+
+@nb.njit
 def _detect_singletons(ids):
     """
-    Detect singleton fixed effects
-    Args:have a
-        ids (np.ndarray): A numpy array of fixed effects.
+    Detect singleton fixed effects in a dataset.
+
+    Args:
+        ids (np.ndarray): A 2D numpy array representing fixed effects, with a
+                          shape of (n_samples, n_features).
+                          Elements should be non-negative integers representing
+                          fixed effect identifiers.
+
     Returns:
-        An array of booleans indicating which observations have a singleton fixed effect.
+        np.ndarray: A boolean array of shape (n_samples,), indicating which
+                    observations have a singleton fixed effect.
+
+    Note:
+        The algorithm iterates over columns to identify fixed effects.
+        After completing each column traversal, it updates the record of
+        non-singleton rows. This approach is preferred as removing an
+        observation in column 'i' may lead to the emergence of new singletons
+        in subsequent columns (i.e., columns > i).
+
+        Since we are operating on columns, we enforce column-major order for
+        the input array. Working on a row-major array leads to considerable
+        performance losses.
     """
+    ids = _prepare_fixed_effects(ids)
+    n_samples, n_features = ids.shape
 
-    N, k = ids.shape
+    max_fixef = np.max(ids)
+    counts = np.empty(max_fixef + 1, dtype=np.uint32)
 
-    singleton_idx = np.full(N, False, dtype=bool)
-    singleton_idx_tmp = np.full(N, False, dtype=bool)
-    singleton_idx_tmp_old = singleton_idx.copy()
-
-    ids_tmp = ids.copy()
+    n_non_singletons = n_samples
+    non_singletons = np.arange(n_non_singletons, dtype=np.uint32)
 
     while True:
-        for x in range(k):
-            col = ids[:, x]
-            col_tmp = ids_tmp[:, x]
+        n_non_singletons_curr = n_non_singletons
 
-            # note that this only "works" as fixed effects are integers from 0, 1, ..., n_fixef -1
-            # and np.bincount orders results in ascending (integer) order
-            counts = np.bincount(col_tmp)
+        for j in range(n_features):
+            col = ids[:, j]
 
-            if np.any(counts == 1):
-                idx = np.where(counts == 1)
+            counts[:] = 0
+            n_singletons = 0
+            for i in range(n_non_singletons):
+                e = col[non_singletons[i]]
+                c = counts[e]
+                # Branchless version of:
+                #
+                # if counts[e] == 1:
+                #     n_singletons -= 1
+                # elif counts[e] == 0:
+                #     n_singletons += 1
+                #
+                n_singletons += (c == 0) - (c == 1)
+                counts[e] += 1
 
-                singleton_idx[np.isin(col, idx)] = True
-                singleton_idx_tmp[np.isin(col_tmp, idx)] = True
+            if not n_singletons:
+                continue
 
-                ids_tmp = ids_tmp[~singleton_idx_tmp, :].astype(ids_tmp.dtype)
-                singleton_idx_tmp = singleton_idx_tmp[~singleton_idx_tmp]
+            cnt = 0
+            for i in range(n_non_singletons):
+                e = col[non_singletons[i]]
+                if counts[e] != 1:
+                    non_singletons[cnt] = non_singletons[i]
+                    cnt += 1
 
+            n_non_singletons = cnt
+
+        if n_non_singletons_curr == n_non_singletons:
             break
 
-        if np.array_equal(singleton_idx_tmp, singleton_idx_tmp_old):
-            break
+    is_singleton = np.ones(n_samples, dtype=np.bool_)
+    for i in range(n_non_singletons):
+        is_singleton[non_singletons[i]] = False
 
-        singleton_idx_tmp_old = singleton_idx_tmp.copy()
-
-    return singleton_idx
+    return is_singleton
