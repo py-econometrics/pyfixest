4eff5da089211c356b8e833b9c946ecf35deae80
Wenzhi Ding <wenzhi.ding@foxmail.com>
2024-02-21 13:09:19 +0800

    fix: fix bug. wrong logic.

--- pyfixest/summarize.py
+++ pyfixest/summarize.py
@@ -153,7 +153,7 @@ def etable(
             elif element == "p":
                 model[coef_fmt_title] += model["Pr(>|t|)"].astype(str)
             elif element in custom_statistics:
-                assert len(custom_statistics[key][i]) != len(
+                assert len(custom_statistics[key][i]) == len(
                     model["Estimate"]
                 ), f"Custom_statistics {key} has unequal length to the number of coefficients in model {i}"
                 model[coef_fmt_title] += pd.Series(
