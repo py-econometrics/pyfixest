a8e8cb59ee3e48712aa9af5b3a0bcec2ecf7da55
Alexander Fischer <alexander-fischer1801@t-online.de>
2024-02-24 18:04:39 +0100

    fix bugs

--- pyfixest/ccv.py
+++ pyfixest/ccv.py
@@ -76,7 +76,7 @@ def ccv(data, depvar, treatment, cluster, xfml = None, seed = None, pk = 1, qk =
 
         Z = rng.choice([False, True], size=N)
         # compute alpha, tau using Z == 0
-        fit_split1 = feols(fml, data[~Z])
+        fit_split1 = feols(fml, data[Z])
         coefs_split = fit_split1._beta_hat
         tau = fit_split1.coef().xs(treatment)
 
@@ -90,7 +90,7 @@ def ccv(data, depvar, treatment, cluster, xfml = None, seed = None, pk = 1, qk =
             ind_m = cluster_vec == m
             Nm = np.sum(ind_m)
             N += Nm
-            ind_m_and_split = ind_m & ~Z
+            ind_m_and_split = ind_m & Z
 
             treatment_nested_in_cluster = (data.loc[ind_m, treatment].nunique() == 1)
             treatment_nested_in_cluster_split = (data.loc[ind_m_and_split, treatment].nunique() == 1)
@@ -117,17 +117,17 @@ def ccv(data, depvar, treatment, cluster, xfml = None, seed = None, pk = 1, qk =
         uhat = Y - X @ coefs_split
         #import pdb; pdb.set_trace()
         #Wbar = np.mean(W[Z])
-        Wbar = np.sum(W * (1 - Z)) / (np.sum((1 - W) * (1 - Z)) + np.sum(W * (1 - Z)))
-        Zavg = np.mean(Z)
+        Wbar = np.mean(W[Z])
+        Zavg = 1 - np.mean(Z)
         Zavg_squared = Zavg**2
         n_adj = N * (Wbar**2) * ((1-Wbar)**2)
 
-        vcov_ccv = 0.0
+        vcov_ccv = 0
         for i, m in enumerate(unique_clusters):
             ind_m = cluster_vec==m
 
-            res_term = (W[ind_m & Z] - Wbar)*uhat[ind_m & Z]
-            tau_term = (tau_ms[i] - tau)*Wbar*(1-Wbar)
+            res_term = (W[ind_m & ~Z] - Wbar)*uhat[ind_m & ~Z]
+            tau_term = (tau_ms[i] - tau)*Wbar*(1.-Wbar)
             diff = res_term - tau_term
 
             #sq_mean = np.mean(diff) ** 2
@@ -135,9 +135,10 @@ def ccv(data, depvar, treatment, cluster, xfml = None, seed = None, pk = 1, qk =
             sq_sum = np.sum(diff)**2
             sum_sq = np.sum(diff**2)
             #vcov_ccv += sq_mean + (1-Zavg) * mean_sq
-            vcov_ccv += ((1 / Zavg_squared) * sq_sum - ((1-Zavg)/Zavg_squared) * sum_sq ) #+ n_adj * pk_term
+            vcov_ccv += (1./(Zavg**2))*sq_sum - ((1.-Zavg)/(Zavg**2))*sum_sq + n_adj * pk_term
 
-        vcov_ccv = vcov_ccv / n_adj + G * pk_term
+        vcov_ccv = vcov_ccv / n_adj
+        return vcov_ccv
 
         vcov_splits[s] = vcov_ccv
 
@@ -173,3 +174,96 @@ def ccv(data, depvar, treatment, cluster, xfml = None, seed = None, pk = 1, qk =
 
     return pd.concat([res_ccv, res_crv1], axis=1).T
 
+
+def compute_CCV(df, cluster, seed, nmx, pk):
+    """ @return: CCV variance for the case qk==1
+        @param df dataframe with columns
+            - 'u' indicator for random split (Z in the paper)
+            - nmx name of the binary indicator W
+            - cluster name of the cluster indicator
+            - 'Y' outcome variable
+        @param u binary variable indicating which split to use for computing the model and which for evaluation
+        @param nmx string with the name of the treatment indicator W
+        @param pk double in (0,1] as described in the paper
+    """
+
+    rng = np.random.default_rng(seed)
+    df["u"] = rng.choice([False, True], size=df.shape[0])
+
+    u1 = df['u']==True
+    u0 = df['u']==False
+    w1 = df[nmx]==1
+    w0 = df[nmx]==0
+
+    # compute alpha, tau using first split
+    alpha = df[u1 & w0]['Y'].mean()
+    tau = df[u1 & w1]['Y'].mean() - df[u1 & w0]['Y'].mean()
+    tau_full = df[w1]['Y'].mean() - df[w0]['Y'].mean()
+
+    # compute for each m
+    tau_ms = {}
+    nm = 'tau_'
+    pk_term = 0
+    for m in df[cluster].unique():
+        ind_m = df[cluster]==m
+        aux1 = df[u1 & ind_m & w1]['Y'].mean()
+        aux0 =  df[u1 & ind_m & w0]['Y'].mean()
+
+        aux1_full = df[ind_m & w1]['Y'].mean()
+        aux0_full =  df[ind_m & w0]['Y'].mean()
+        aux_tau = aux1 - aux0
+        aux_tau_full = aux1_full - aux0_full
+
+        if (np.isnan(aux1)) or (np.isnan(aux0)):
+            aux_tau = tau
+
+        aux_nm = nm + str(m)
+        tau_ms[aux_nm] = aux_tau
+
+        # compute the pk term in u0
+        Nm = df[ind_m].shape[0]
+        aux_pk = Nm*((aux_tau_full-tau)**2)
+        pk_term = pk_term + aux_pk
+
+    # compute the residuals
+    df['resU'] = df['Y'] - alpha - df[nmx]*tau
+
+    # Wbar
+    Wbar = df[u1][nmx].mean()
+    #Wbar = df[nmx].mean() # to match Guido
+
+    # pk term
+    pk_term = pk_term*(1.-pk)/df.shape[0]
+
+
+    # compute avg Z
+    Zavg = (np.sum(df['u']==False))/df.shape[0]
+
+    # compute the normalized CCV using second split
+    n = (df.shape[0]*(Wbar**2)*((1.-Wbar)**2))
+
+    sum_CCV = 0
+    for m in df[cluster].unique():
+        ind_m = df[cluster]==m
+        df_m = df[u0 & ind_m]
+        aux_nm = nm + str(m)
+
+        # tau term
+        tau_term = (tau_ms[aux_nm] - tau)*Wbar*(1.-Wbar)
+
+        # Residual term
+        res_term = (df_m[nmx] - Wbar)*df_m['resU']
+
+        # square of sums
+        sq_sum = np.sum(res_term - tau_term)**2
+
+        # sum of squares
+        sum_sq = np.sum((res_term - tau_term)**2)
+
+        # compute CCV
+        sum_CCV += (1./(Zavg**2))*sq_sum - ((1.-Zavg)/(Zavg**2))*sum_sq + n*pk_term
+
+    # normalize
+    V_CCV = sum_CCV / n
+
+    return V_CCV
