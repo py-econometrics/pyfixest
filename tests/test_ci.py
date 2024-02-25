3630a95c5181200df748b58d1ef14a2e0caa0d77
Alexander Fischer <alexander-fischer1801@t-online.de>
2024-02-25 11:21:49 +0100

    lint

--- tests/test_ci.py
+++ tests/test_ci.py
@@ -1,11 +1,12 @@
+import doubleml as dml
 import numpy as np
 import pandas as pd
+from sklearn.base import clone
+from sklearn.linear_model import LinearRegression
 
 from pyfixest.estimation import feols
 from pyfixest.utils import get_data
-import doubleml as dml
-from sklearn.base import clone
-from sklearn.linear_model import LinearRegression
+
 
 def test_confint():
     """Test the confint method of the feols class."""
@@ -40,23 +41,25 @@ def test_confint():
 
 
 def test_against_doubleml():
+    """Test joint CIs against DoubleML."""
+    rng = np.random.default_rng(2002)
+    n_obs = 5_000
+    n_vars = 100
+    X = rng.normal(size=(n_obs, n_vars))
+    theta = np.array([3.0, 3.0, 3.0])
+    y = np.dot(X[:, :3], theta) + rng.standard_normal(size=(n_obs,))
 
-        """Test joint CIs against DoubleML."""
+    dml_data = dml.DoubleMLData.from_arrays(X[:, 10:], y, X[:, :10])
+    learner = LinearRegression()
+    ml_l = clone(learner)
+    ml_m = clone(learner)
+    dml_plr = dml.DoubleMLPLR(dml_data, ml_l, ml_m)
+    dml_res = dml_plr.fit().bootstrap(n_rep_boot=10_000).confint(joint=True)
 
-        rng = np.random.default_rng(2002)
-        n_obs = 5_000
-        n_vars = 100
-        X = rng.normal(size=(n_obs, n_vars))
-        theta = np.array([3., 3., 3.])
-        y = np.dot(X[:, :3], theta) + rng.standard_normal(size=(n_obs,))
+    df = pd.DataFrame(
+        np.c_[y, X], columns=["y"] + ["X_" + str(x) for x in range(n_vars)]
+    )
+    m = feols(f"y ~ -1 + {'+'.join(['X_'+str(x) for x in range(n_vars)])}", df)
+    pyfixest_res = m.confint(keep="X_.$", nboot=10_000, joint=True)
 
-        dml_data = dml.DoubleMLData.from_arrays(X[:, 10:], y, X[:, :10])
-        learner = LinearRegression(); ml_l = clone(learner); ml_m = clone(learner)
-        dml_plr = dml.DoubleMLPLR(dml_data, ml_l, ml_m)
-        dml_res = dml_plr.fit().bootstrap(n_rep_boot = 10_000).confint(joint=True)
-
-        df = pd.DataFrame(np.c_[y, X], columns = ['y'] + ['X_'+str(x) for x in range(n_vars)])
-        m = feols(f"y ~ -1 + {'+'.join(['X_'+str(x) for x in range(n_vars)])}", df)
-        pyfixest_res = m.confint(keep = 'X_.$', nboot = 10_000, joint = True)
-
-        assert np.all(np.abs(dml_res.values - pyfixest_res.values) < 1e-2)
+    assert np.all(np.abs(dml_res.values - pyfixest_res.values) < 1e-2)
