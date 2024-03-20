3b474241c77befe5d55366d718d32190b5236df2
Alexander Fischer <alexander-fischer1801@t-online.de>
2024-03-20 20:01:34 +0100

    update names in test_formulas

--- tests/test_formulas.py
+++ tests/test_formulas.py
@@ -1,59 +1,59 @@
 from pyfixest.estimation.FormulaParser import (
     _find_multiple_estimation_syntax,
-    _pack_to_fml,
-    _unpack_fml,
+    _dict_to_list_of_formulas,
+    _input_formula_to_dict,
 )
 
 
-def test_unpack_fml():
-    assert _unpack_fml("a+b+c") == {"constant": ["a", "b", "c"]}
+def test_input_formula_to_dict():
+    assert _input_formula_to_dict("a+b+c") == {"constant": ["a", "b", "c"]}
 
-    assert _unpack_fml("sw(x,y)") == {"constant": [], "sw": ["x", "y"]}
+    assert _input_formula_to_dict("sw(x,y)") == {"constant": [], "sw": ["x", "y"]}
 
-    assert _unpack_fml("a+sw0(x,y)+d") == {"constant": ["a", "d"], "sw0": ["x", "y"]}
+    assert _input_formula_to_dict("a+sw0(x,y)+d") == {"constant": ["a", "d"], "sw0": ["x", "y"]}
 
-    assert _unpack_fml("csw(x,y)") == {"constant": [], "csw": ["x", "y"]}
+    assert _input_formula_to_dict("csw(x,y)") == {"constant": [], "csw": ["x", "y"]}
 
-    assert _unpack_fml("csw0(x,y,z)") == {"constant": [], "csw0": ["x", "y", "z"]}
+    assert _input_formula_to_dict("csw0(x,y,z)") == {"constant": [], "csw0": ["x", "y", "z"]}
 
-    assert _unpack_fml("a+b+csw0(x,y,z)") == {
+    assert _input_formula_to_dict("a+b+csw0(x,y,z)") == {
         "constant": ["a", "b"],
         "csw0": ["x", "y", "z"],
     }
 
 
-def test_pack_to_fml():
-    # assert _pack_to_fml([]) == []
+def test_dict_to_list_of_formulas():
+    # assert _dict_to_list_of_formulas([]) == []
 
-    assert _pack_to_fml({"constant": ["x", "y"], "sw0": ["a", "b"]}) == [
+    assert _dict_to_list_of_formulas({"constant": ["x", "y"], "sw0": ["a", "b"]}) == [
         "x+y",
         "x+y+a",
         "x+y+b",
     ]
-    assert _pack_to_fml({"constant": [], "sw0": ["a", "b"]}) == ["0", "a", "b"]
-    assert _pack_to_fml({"constant": ["x", "y"], "sw0": []}) == ["x+y"]
+    assert _dict_to_list_of_formulas({"constant": [], "sw0": ["a", "b"]}) == ["0", "a", "b"]
+    assert _dict_to_list_of_formulas({"constant": ["x", "y"], "sw0": []}) == ["x+y"]
 
-    assert _pack_to_fml({"constant": ["x", "y"], "sw": ["a", "b"]}) == [
+    assert _dict_to_list_of_formulas({"constant": ["x", "y"], "sw": ["a", "b"]}) == [
         "x+y+a",
         "x+y+b",
     ]
-    assert _pack_to_fml({"constant": [], "sw": ["a", "b"]}) == ["a", "b"]
-    assert _pack_to_fml({"constant": ["x", "y"], "sw": []}) == ["x+y"]
+    assert _dict_to_list_of_formulas({"constant": [], "sw": ["a", "b"]}) == ["a", "b"]
+    assert _dict_to_list_of_formulas({"constant": ["x", "y"], "sw": []}) == ["x+y"]
 
-    assert _pack_to_fml({"constant": ["x", "y"], "csw0": ["a", "b"]}) == [
+    assert _dict_to_list_of_formulas({"constant": ["x", "y"], "csw0": ["a", "b"]}) == [
         "x+y",
         "x+y+a",
         "x+y+a+b",
     ]
-    assert _pack_to_fml({"constant": [], "csw0": ["a", "b"]}) == ["0", "a", "a+b"]
-    assert _pack_to_fml({"constant": ["x", "y"], "csw0": []}) == ["x+y"]
+    assert _dict_to_list_of_formulas({"constant": [], "csw0": ["a", "b"]}) == ["0", "a", "a+b"]
+    assert _dict_to_list_of_formulas({"constant": ["x", "y"], "csw0": []}) == ["x+y"]
 
-    assert _pack_to_fml({"constant": ["x", "y"], "csw": ["a", "b"]}) == [
+    assert _dict_to_list_of_formulas({"constant": ["x", "y"], "csw": ["a", "b"]}) == [
         "x+y+a",
         "x+y+a+b",
     ]
-    assert _pack_to_fml({"constant": [], "csw": ["a", "b"]}) == ["a", "a+b"]
-    assert _pack_to_fml({"constant": ["x", "y"], "csw": []}) == ["x+y"]
+    assert _dict_to_list_of_formulas({"constant": [], "csw": ["a", "b"]}) == ["a", "a+b"]
+    assert _dict_to_list_of_formulas({"constant": ["x", "y"], "csw": []}) == ["x+y"]
 
 
 def test_find_multiple_estimation_syntax_no_match():
