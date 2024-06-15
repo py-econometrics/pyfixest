b61df52321de8cc73b3b99f4d4b2f7e97fa4fdc1
Alexander Fischer <alexander-fischer1801@t-online.de>
2024-06-15 20:10:47 +0200

    initial refactor of the vcov method, add sub methods

---
+++ pyfixest/estimation/vcov_utils.py
@@ -0,0 +1,87 @@
+import numba as nb
+import numpy as np
+
+
+def _compute_bread(_is_iv, _tXZ, _tZZinv, _tZX, _hessian):
+    return np.linalg.inv(_tXZ @ _tZZinv @ _tZX) if _is_iv else np.linalg.inv(_hessian)
+
+
+# CODE from Styfen Schaer (@styfenschaer)
+@nb.njit(parallel=False)
+def bucket_argsort(arr: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Sorts the input array using the bucket sort algorithm.
+
+    Parameters
+    ----------
+    arr : array_like
+        An array_like object that needs to be sorted.
+
+    Returns
+    -------
+    array_like
+        A sorted copy of the input array.
+
+    Raises
+    ------
+    ValueError
+        If the input is not an array_like object.
+
+    Notes
+    -----
+    The bucket sort algorithm works by distributing the elements of an array
+    into a number of buckets. Each bucket is then sorted individually, either
+    using a different sorting algorithm, or by recursively applying the bucket
+    sorting algorithm.
+    """
+    counts = np.zeros(arr.max() + 1, dtype=np.uint32)
+    for i in range(arr.size):
+        counts[arr[i]] += 1
+
+    locs = np.empty(counts.size + 1, dtype=np.uint32)
+    locs[0] = 0
+    pos = np.empty(counts.size, dtype=np.uint32)
+    for i in range(counts.size):
+        locs[i + 1] = locs[i] + counts[i]
+        pos[i] = locs[i]
+
+    args = np.empty(arr.size, dtype=np.uint32)
+    for i in range(arr.size):
+        e = arr[i]
+        args[pos[e]] = i
+        pos[e] += 1
+
+    return args, locs
+
+
+# CODE from Styfen Schaer (@styfenschaer)
+@nb.njit(parallel=False)
+def _crv1_meat_loop(
+    _Z: np.ndarray,
+    weighted_uhat: np.ndarray,
+    clustid: np.ndarray,
+    cluster_col: np.ndarray,
+) -> np.ndarray:
+    k = _Z.shape[1]
+    dtype = _Z.dtype
+    meat = np.zeros((k, k), dtype=dtype)
+
+    g_indices, g_locs = bucket_argsort(cluster_col)
+
+    score_g = np.empty((k, 1), dtype=dtype)
+    meat_i = np.empty((k, k), dtype=dtype)
+
+    for i in range(clustid.size):
+        g = clustid[i]
+        start = g_locs[g]
+        end = g_locs[g + 1]
+        g_index = g_indices[start:end]
+
+        Zg = _Z[g_index]
+        ug = weighted_uhat[g_index]
+
+        np.dot(Zg.T, ug, out=score_g)
+        np.outer(score_g, score_g, out=meat_i)
+        meat += meat_i
+
+    return meat
