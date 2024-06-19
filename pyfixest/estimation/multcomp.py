106f5036b5239460a652d03c90b4d693da1ac3b4
Jaehyung Kim <jhkim@nyu.edu>
2024-06-18 20:24:42 -0400

    Minor change to rwolf function and add a test code for testing whether the "sampling_method" feature works

--- pyfixest/estimation/multcomp.py
+++ pyfixest/estimation/multcomp.py
@@ -177,7 +177,6 @@ def rwolf(
     pval = _get_rwolf_pval(t_stats, boot_t_stats)
 
     all_model_stats.loc["RW Pr(>|t|)"] = pval
-    # all_model_stats.loc["Sampling Method"] = sampling_method
     all_model_stats.columns = pd.Index([f"est{i}" for i, _ in enumerate(models)])
     return all_model_stats
 
