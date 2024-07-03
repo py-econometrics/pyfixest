fe51d55facf884ca5cbe46c14c2004247f8d54ea
Alexander Fischer <alexander-fischer1801@t-online.de>
2024-07-03 20:47:42 +0200

    Update feols_.py
    add doc string for `alpha` argument to `tidy()`

--- pyfixest/estimation/feols_.py
+++ pyfixest/estimation/feols_.py
@@ -1497,6 +1497,12 @@ class Feols:
         Return a tidy pd.DataFrame with the point estimates, standard errors,
         t-statistics, and p-values.
 
+        Parameters 
+        ----------
+        alpha: Optional[float]
+            The significance level for the confidence intervals. If None, 
+            computes a 95% confidence interval (`alpha = 0.05`). 
+        
         Returns
         -------
         tidy_df : pd.DataFrame
