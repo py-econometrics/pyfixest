cd2bdbdc6dd0f3502ee222c5d10521cf95ecbfcc
Apoorva Lal <lal.apoorva@gmail.com>
2024-07-28 13:15:35 -0700

    appease codecov

--- tests/test_others.py
+++ tests/test_others.py
@@ -73,3 +73,27 @@ def test_coef_update():
     )
 
     np.testing.assert_allclose(updated_coefs, full_coefs)
+
+def test_coef_update_inplace():
+    data = get_data()
+    data_subsample = data.sample(frac=0.3)
+    m = feols("Y ~ X1 + X2", data=data_subsample)
+    new_points_id = np.random.choice(
+        list(set(data.index) - set(data_subsample.index)), 5
+    )
+    X_new, y_new = (
+        np.c_[
+            data.loc[new_points_id][["X1", "X2"]].values # only pass columns; let `update` add the intercept
+        ],
+        data.loc[new_points_id]["Y"].values,
+    )
+    m.update(X_new, y_new, inplace=True)
+    full_coefs = (
+        feols(
+            "Y ~ X1 + X2",
+            data=data.loc[data_subsample.index.append(pd.Index(new_points_id))],
+        )
+        .coef()
+        .values
+    )
+    np.testing.assert_allclose(m.coef().values, full_coefs)
