e4220bbf6dcbca6cf5479721c1fcac154660ea8e
Rafi Mikail <rafi.mikail-allofresh@contractor.bukalapak.com>
2024-08-24 23:48:00 +0700

    adding units to plot features, xlim, and ylim

--- pyfixest/did/visualize.py
+++ pyfixest/did/visualize.py
@@ -22,6 +22,9 @@ def panelview(
     title: Optional[str] = None,
     legend: Optional[bool] = False,
     ax: Optional[plt.Axes] = None,
+    xlim: Optional[tuple] = None, 
+    ylim: Optional[tuple] = None,
+    units_to_plot: Optional[list] = None
 ) -> None:
     """
     Generate a panel view of the treatment variable over time for each unit.
@@ -96,48 +99,57 @@ def panelview(
     ```
     """
     if type == "outcome" and outcome:
+        if units_to_plot:
+            data = data[data[unit].isin(units_to_plot)]
+        data_pivot = data.pivot(index=unit, columns=time, values=outcome)
         if subsamp:
-            unique_units = data[unit].unique()
-            sampled_units = np.random.choice(unique_units, size=subsamp, replace=False)
-            data = data[data[unit].isin(sampled_units)]
+            data_pivot = data_pivot.sample(subsamp)
         if collapse_to_cohort:
-            treatment_starts = data.groupby(unit).apply(lambda x: x[x[treat] == True][time].min())
+            treatment_starts = data.groupby(unit).apply(lambda x: x[x[treat] == True][time].min(), include_groups=False)
             treatment_starts = treatment_starts.reset_index(name='treatment_start')
-            treatment_starts = treatment_starts.drop_duplicates(subset='treatment_start')
-            data = data[data[unit].isin(treatment_starts[unit])]
+            data = data.merge(treatment_starts, on = unit, how='left')
+            data_agg = data.groupby(["treatment_start","year"], dropna=False)["turnout"].mean().reset_index()
+            data_agg[treat] = data_agg.apply(lambda row: row['year'] >= row['treatment_start'] if pd.notna(row['treatment_start']) else False, axis=1)
+            data_agg = data_agg.rename(columns={'treatment_start': unit})
+            data = data_agg.copy()
+            data_pivot = data_agg.pivot(index=unit, columns=time, values=outcome)
         if not ax:
             f, ax = plt.subplots(figsize=figsize, dpi = 300)
-        for unit_id in data[unit].unique():
-            unit_data = data[data[unit] == unit_id]
-            treatment_times = unit_data[unit_data[treat] == True][time]
+        for unit_id in data_pivot.index:
+            unit_data = data_pivot.loc[unit_id]
+            treatment_times = data[(data[unit] == unit_id) & (data[treat] == True)][time]
             
             # If the unit never receives treatment, plot the line in grey
             if treatment_times.empty:
-                ax.plot(unit_data[time], unit_data[outcome], color="#999999", linewidth=0.5, alpha = 0.5)
+                ax.plot(unit_data.index, unit_data.values, color="#999999", linewidth=0.5, alpha=0.5)
             else:
                 treatment_start = treatment_times.min()
 
                 # Plot the entire line with the initial color (orange), then change to red after treatment
                 ax.plot(
-                    unit_data[time],
-                    unit_data[outcome],
+                    unit_data.index,
+                    unit_data.values,
                     color="#FF8343",
-                    linewidth=0.5,  # Thinner line
+                    linewidth=0.5,
                     label=f"Unit {unit_id}" if legend else None,
-                    alpha = 0.5
+                    alpha=0.5
                 )
                 ax.plot(
-                    unit_data[unit_data[time] >= treatment_start][time],
-                    unit_data[unit_data[time] >= treatment_start][outcome],
+                    unit_data.index[unit_data.index >= treatment_start],
+                    unit_data.values[unit_data.index >= treatment_start],
                     color="#ff0000",
-                    linewidth=0.9,  
-                    alpha = 0.5
+                    linewidth=0.9,
+                    alpha=0.5
                 )
 
         ax.set_xlabel(xlab if xlab else time)
         ax.set_ylabel(ylab if ylab else outcome)
-        ax.set_title(title if title else "Outcome over Time with Treatment Effect", fontweight='bold')  # Bold title
+        ax.set_title(title if title else "Outcome over Time with Treatment Effect", fontweight='bold')
         ax.grid(True, color="#e0e0e0", linewidth=0.3, linestyle='-')
+        if xlim:
+            ax.set_xlim(xlim)
+        if ylim:
+            ax.set_ylim(ylim)
         if legend:
             custom_lines = [
                 plt.Line2D([0], [0], color="#999999", lw=1.5),
