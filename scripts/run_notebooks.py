3633d916b0ac87d3eb6829626b61e7227b50ac79
juanitorduz <juan.orduz@wolt.com>
2024-09-04 19:20:17 +0200

    test notebooks init

---
+++ scripts/run_notebooks.py
@@ -0,0 +1,46 @@
+"""Script to run all notebooks in the docs/source/notebooks directory."""
+
+import logging
+from pathlib import Path
+
+import papermill
+from joblib import Parallel, delayed
+from tqdm import tqdm
+
+KERNEL_NAME: str = "python3"
+DOC_SOURCE = Path("docs")
+NOTEBOOKS_PATH = DOC_SOURCE / "notebooks"
+NOTEBOOKS: list[Path] = list(NOTEBOOKS_PATH.glob("*/*.ipynb"))
+
+
+def setup_logging() -> None:
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
+
+
+def get_cwd_from_notebook_path(notebook_path: Path) -> str:
+    return str(notebook_path).rsplit("/", 1)[0]
+
+
+def run_notebook(notebook_path: Path) -> None:
+    cwd = get_cwd_from_notebook_path(notebook_path)
+    logging.info(f"Running notebook: {notebook_path.name}")
+    papermill.execute_notebook(
+        input_path=str(notebook_path),
+        output_path=None,
+        kernel_name=KERNEL_NAME,
+        cwd=cwd,
+    )
+
+
+if __name__ == "__main__":
+    setup_logging()
+    logging.info("Starting notebook runner")
+    logging.info(f"Notebooks to run: {NOTEBOOKS}")
+    Parallel(n_jobs=-1)(
+        delayed(run_notebook)(notebook_path) for notebook_path in tqdm(NOTEBOOKS)
+    )
+
+    logging.info("Notebooks run successfully!")
