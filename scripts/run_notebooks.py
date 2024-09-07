da6457c45efefd6eb9a2e4d72ad6c71c9a012707
Alexander Fischer <alexander-fischer1801@t-online.de>
2024-09-07 18:19:04 +0200

    Run more tests (#603)

--- scripts/run_notebooks.py
+++ scripts/run_notebooks.py
@@ -1,6 +1,6 @@
 """Script to run all notebooks example notebooks.
 
-Taken from: https://github.com/pymc-labs/pymc-marketing/blob/main/scripts/run_notebooks/runner.py
+Taken from: https://github.com/pymc-labs/pymc-marketing/blob/main/scripts/_run_notebooks/runner.py
 """
 
 import logging
@@ -22,19 +22,19 @@ NOTEBOOKS: list[Path] = [
 ]
 
 
-def setup_logging() -> None:
+def _setup_logging() -> None:
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     )
 
 
-def get_cwd_from_notebook_path(notebook_path: Path) -> str:
+def _get_cwd_from_notebook_path(notebook_path: Path) -> str:
     return str(notebook_path).rsplit("/", 1)[0]
 
 
-def run_notebook(notebook_path: Path) -> None:
-    cwd = get_cwd_from_notebook_path(notebook_path)
+def _run_notebook(notebook_path: Path) -> None:
+    cwd = _get_cwd_from_notebook_path(notebook_path)
     logging.info(f"Running notebook: {notebook_path.name}")
     papermill.execute_notebook(
         input_path=str(notebook_path),
@@ -45,11 +45,11 @@ def run_notebook(notebook_path: Path) -> None:
 
 
 if __name__ == "__main__":
-    setup_logging()
+    _setup_logging()
     logging.info("Starting notebook runner")
     logging.info(f"Notebooks to run: {NOTEBOOKS}")
     Parallel(n_jobs=-1)(
-        delayed(run_notebook)(notebook_path) for notebook_path in tqdm(NOTEBOOKS)
+        delayed(_run_notebook)(notebook_path) for notebook_path in tqdm(NOTEBOOKS)
     )
 
     logging.info("Notebooks run successfully!")
