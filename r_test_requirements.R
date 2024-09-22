207263d19f25c586872930ed17502c28e2660e74
Alexander Fischer <alexander-fischer1801@t-online.de>
2024-09-22 12:31:52 +0200

    install r packages by hand

--- r_test_requirements.R
+++ r_test_requirements.R
@@ -1,11 +1,9 @@
 # note: fixest and broom are installed via conda
 install.packages(
     c('fixest', 'broom','clubSandwich', 'did2s', 'wildrwolf', 'reticulate', 'ivDiag'),
-    repos='https://cran.rstudio.com',
-    type = "binary"
+    repos='https://cran.rstudio.com'
 );
 install.packages(
     'ritest',
-    repos = c('https://grantmcdermott.r-universe.dev', 'https://cloud.r-project.org'),
-    type = "binary"
+    repos = c('https://grantmcdermott.r-universe.dev', 'https://cloud.r-project.org')
 );
