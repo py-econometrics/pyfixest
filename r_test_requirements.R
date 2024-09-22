caee64c1e99346fcfef96fde21b281b3b1ecb234
Alexander Fischer <alexander-fischer1801@t-online.de>
2024-09-22 12:28:59 +0200

    revert to r2u

--- r_test_requirements.R
+++ r_test_requirements.R
@@ -1,18 +1,11 @@
+# note: fixest and broom are installed via conda
 install.packages(
-  "fixest",
-  repos = "https://packagemanager.rstudio.com/all/__linux__/focal/latest"
-)
-
-install.packages('broom', repos = c('https://tidymodels.r-universe.dev', 'https://cloud.r-project.org'))
-
-install.packages('clubSandwich', repos = c('https://jepusto.r-universe.dev', 'https://cloud.r-project.org'))
-
-install.packages('did2s', repos = c('https://kylebutts.r-universe.dev', 'https://cloud.r-project.org'))
-
-install.packages('wildrwolf', repos = c('https://s3alfisc.r-universe.dev', 'https://cloud.r-project.org'))
-
-install.packages('reticulate', repos = c('https://rstudio.r-universe.dev', 'https://cloud.r-project.org'))
-
-install.packages('ivDiag', repos = 'https://cloud.r-project.org')
-
-install.packages('ritest', repos = c('https://grantmcdermott.r-universe.dev', 'https://cloud.r-project.org'))
+    c('fixest', 'broom','clubSandwich', 'did2s', 'wildrwolf', 'reticulate', 'ivDiag'),
+    repos='https://cran.rstudio.com',
+    type = "binary"
+);
+install.packages(
+    'ritest',
+    repos = c('https://grantmcdermott.r-universe.dev', 'https://cloud.r-project.org'),
+    type = "binary"
+);
