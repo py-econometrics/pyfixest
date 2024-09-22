fddc0dad670617536913c4ed15b3cd16bf6af1b0
Alexander Fischer <alexander-fischer1801@t-online.de>
2024-09-22 12:03:29 +0200

    install compiled linux from r universe

--- r_test_requirements.R
+++ r_test_requirements.R
@@ -1,9 +1,5 @@
 # note: fixest and broom are installed via conda
 install.packages(
-    c('fixest', 'broom','clubSandwich', 'did2s', 'wildrwolf', 'reticulate', 'ivDiag'),
-    repos='https://cran.rstudio.com',
-);
-install.packages(
-    'ritest',
-    repos = c('https://grantmcdermott.r-universe.dev', 'https://cloud.r-project.org'),
-);
+    c('fixest', 'broom','clubSandwich', 'did2s', 'wildrwolf', 'reticulate', 'ivDiag', 'ritest'),
+    repos = c('https://ropensci.r-universe.dev')
+)
