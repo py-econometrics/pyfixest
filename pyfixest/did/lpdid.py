31a714b1ecf8446f9eff88d8df09f81c3d3574a1
Marc-Andre <marcandrechenier@gmail.com>
2024-10-27 21:20:21 +0100

    Fix lpdid argument vcov

--- pyfixest/did/lpdid.py
+++ pyfixest/did/lpdid.py
@@ -6,7 +6,9 @@ import pandas as pd
 from pyfixest.did.did import DID
 from pyfixest.estimation.estimation import feols
 from pyfixest.estimation.feols_ import Feols
+from pyfixest.estimation.literals import vcov_type_options
 from pyfixest.report.visualize import _coefplot
+
 
 
 class LPDID(DID):
@@ -51,7 +53,7 @@ class LPDID(DID):
         xfml: str,
         att: bool,
         cluster: str,
-        vcov: Optional[Union[str, dict[str, str]]] = None,
+        vcov: Optional[Union[vcov_type_options, dict[str, str]]] = None,
         pre_window: Optional[int] = None,
         post_window: Optional[int] = None,
         never_treated: Optional[int] = 0,
@@ -201,7 +203,7 @@ def _lpdid_estimate(
     pre_window: int,
     post_window: int,
     att: bool = True,
-    vcov: Optional[Union[str, dict[str, str]]] = None,
+    vcov: Optional[Union[vcov_type_options, dict[str, str]]] = None,
     xfml: Optional[str] = None,
 ) -> pd.DataFrame:
     """
