e50a3ce5264030001f8800f46514cbc0de0fe318
Marc-Andre <marcandrechenier@gmail.com>
2024-10-27 21:15:02 +0100

    Adding docstring

--- pyfixest/estimation/literals.py
+++ pyfixest/estimation/literals.py
@@ -8,6 +8,25 @@ solver_options = Literal["np.linalg.solve", "np.linalg.lstsq"]
 
 
 def validate_literal_argument(arg: Any, literal: Literal) -> None:
+    """
+    Validate if the given argument matches one of the allowed literal types.
+
+    This function checks whether the provided `arg` is among the valid types
+    returned by `get_args(literal)`. If not, it raises a ValueError with an
+    appropriate error message.
+
+    Parameters
+    ----------
+    arg : Any
+        The argument to validate.
+    literal : Literal[ValidTypes]
+        A Literal type that defines the allowed values for `arg`.
+
+    Raises
+    ------
+    ValueError
+        If `arg` is not one of the valid types defined by `literal`.
+    """
     valid_types = get_args(literal)
 
     if arg not in valid_types:
