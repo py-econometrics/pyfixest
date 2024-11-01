56c56b232df5a74b0dce2c8da677aa183c25abc7
Marc-Andre <marcandrechenier@gmail.com>
2024-11-01 17:37:53 +0100

    Check length of valid_types instead looking for generic literal type

--- pyfixest/estimation/literals.py
+++ pyfixest/estimation/literals.py
@@ -1,4 +1,4 @@
-from typing import Any, Literal, _LiteralGenericAlias, get_args
+from typing import Literal, get_args, Any
 
 PredictionType = Literal["response", "link"]
 VcovTypeOptions = Literal["iid", "hetero", "HC1", "HC2", "HC3"]
@@ -7,7 +7,7 @@ FixedRmOptions = Literal["singleton", "none"]
 SolverOptions = Literal["np.linalg.solve", "np.linalg.lstsq"]
 
 
-def _validate_literal_argument(arg: Any, literal: _LiteralGenericAlias) -> None:
+def _validate_literal_argument(arg: Any, literal: Any) -> None:
     """
     Validate if the given argument matches one of the allowed literal types.
 
@@ -19,18 +19,22 @@ def _validate_literal_argument(arg: Any, literal: _LiteralGenericAlias) -> None:
     ----------
     arg : Any
         The argument to validate.
-    literal : _LiteralGenericAlias
+    literal : Any
         A Literal type that defines the allowed values for `arg`.
 
     Raises
     ------
+    TypeError
+        If `literal` does not have valid types.
     ValueError
         If `arg` is not one of the valid types defined by `literal`.
     """
-    if type(literal) is _LiteralGenericAlias:
-        valid_types = get_args(literal)
-    else:
-        raise TypeError(f"{literal} must be of Literal[...] type")
+    valid_types = get_args(literal)
+
+    if len(valid_types) < 1:
+        raise TypeError(
+            f"{literal} must be a Literal[...] type argument with least one type"
+        )
 
     if arg not in valid_types:
         raise ValueError(f"Invalid argument. Expecting one of {valid_types}. Got {arg}")
