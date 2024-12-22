c293decf8c50944e365c092f0de4e868b3beae14
Alexander Fischer <alexander-fischer1801@t-online.de>
2024-12-22 18:23:00 +0100

    type hints

--- pyfixest/estimation/feglm.py
+++ pyfixest/estimation/feglm.py
@@ -5,12 +5,17 @@ import numpy as np
 import pandas as pd
 from scipy.stats import norm
 
+from pyfixest.errors import (
+    NonConvergenceError,
+)
 from pyfixest.estimation.demean_ import demean
 from pyfixest.estimation.fepois_ import Fepois
 from pyfixest.estimation.FormulaParser import FixestFormula
 
 
 class Feglm(Fepois, ABC):
+    "Abstract base class for the estimation of a fixed-effects GLM model."
+
     def __init__(
         self,
         FixestFormula: FixestFormula,
@@ -56,7 +61,7 @@ class Feglm(Fepois, ABC):
 
         self._method = "feglm"
 
-    def get_fit(self):
+    def _get_fit(self):
         "Fit the GLM model via iterated weighted least squares."
         _Y = self._Y
         _X = self._X
@@ -72,19 +77,19 @@ class Feglm(Fepois, ABC):
 
         beta = np.zeros(_X.shape[1])
         eta = np.zeros(_N)
-        mu = self.get_mu(theta=eta)
-        deviance_old = self.compute_deviance(_Y.flatten(), mu)
+        mu = self._get_mu(theta=eta)
+        deviance_old = self._get_deviance(_Y.flatten(), mu)
 
         for r in range(_maxiter):
-            # Step 1: compute weights w_tilde(r-1) and v(r-1) (eq. 2.5)
-            detadmu = self.update_detadmu(mu=mu)
-            v = self.update_v(y=_Y.flatten(), mu=mu, detadmu=detadmu)
-            W = self.update_W(mu=mu)
+            # Step 1: _get weights w_tilde(r-1) and v(r-1) (eq. 2.5)
+            detadmu = self._update_detadmu(mu=mu)
+            # v = self._update_v(y=_Y.flatten(), mu=mu, detadmu=detadmu)
+            W = self._update_W(mu=mu)
 
-            # Step 2: compute v_tilde(r-1) and X_tilde(r-1) (eq. 3.2)
-            W_tilde = self.update_W_tilde(W=W)
-            X_tilde = self.update_X_tilde(W_tilde=W_tilde, X=_X)
-            v_tilde = self.update_v_tilde(
+            # Step 2: _get v_tilde(r-1) and X_tilde(r-1) (eq. 3.2)
+            W_tilde = self._update_W_tilde(W=W)
+            X_tilde = self._update_X_tilde(W_tilde=W_tilde, X=_X)
+            v_tilde = self._update_v_tilde(
                 y=_Y.flatten(), mu=mu, W_tilde=W_tilde.flatten(), detadmu=detadmu
             )
 
@@ -97,32 +102,34 @@ class Feglm(Fepois, ABC):
                 tol=_fixef_tol,
             )
 
-            # Step 4: compute (beta(r) - beta(r-1)) and check for convergence, update beta(r-1) s(eq. 3.5)
-            beta_update_diff = self.update_beta_diff(
+            # Step 4: compute (beta(r) - beta(r-1)) and check for convergence, _update beta(r-1) s(eq. 3.5)
+            beta_update_diff = self._update_beta_diff(
                 X_dotdot=X_dotdot, v_dotdot=v_dotdot
             )
 
-            # Step 5: update using step halfing (if required)
+            # Step 5: _update using step halfing (if required)
             mu_old = mu.copy()
-            beta, eta, mu, deviance, crit, step_accepted = self.update_eta_step_halfing(
-                Y=_Y,
-                beta=beta,
-                eta=eta,
-                mu=mu,
-                deviance=deviance_old,
-                beta_update_diff=beta_update_diff,
-                W_tilde=W_tilde.flatten(),
-                v_tilde=v_tilde,
-                v_dotdot=v_dotdot,
-                X_dotdot=X_dotdot,
-                deviance_old=deviance_old,
-                step_halfing_tolerance=1e-12,
+            beta, eta, mu, deviance, crit, step_accepted = (
+                self._update_eta_step_halfing(
+                    Y=_Y,
+                    beta=beta,
+                    eta=eta,
+                    mu=mu,
+                    deviance=deviance_old,
+                    beta_update_diff=beta_update_diff,
+                    W_tilde=W_tilde.flatten(),
+                    v_tilde=v_tilde,
+                    v_dotdot=v_dotdot,
+                    X_dotdot=X_dotdot,
+                    deviance_old=deviance_old,
+                    step_halfing_tolerance=1e-12,
+                )
             )
 
             print("beta:", beta)
 
             deviance_old = deviance.copy()
-            converged = self.stop_iterating(crit=crit, tol=_tol, r=r, maxiter=_maxiter)
+            converged = self._stop_iterating(crit=crit, tol=_tol, r=r, maxiter=_maxiter)
             if converged:
                 break
 
@@ -132,7 +139,7 @@ class Feglm(Fepois, ABC):
         # (Y - self._Y_hat)
         # needed for the calculation of the vcov
 
-        # update for inference
+        # _update for inference
         self._weights = mu_old
         self._irls_weights = mu
         # if only one dim
@@ -159,94 +166,115 @@ class Feglm(Fepois, ABC):
             self._convergence = True
 
     @abstractmethod
-    def compute_deviance(self, y, mu):
+    def _get_deviance(self, y: np.ndarray, mu: np.ndarray) -> np.ndarray:
         "Compute the deviance for the GLM family."
         pass
 
     @abstractmethod
-    def get_dispersion_phi(self, theta):
+    def _get_dispersion_phi(self, theta: np.ndarray) -> int:
         "Get the dispersion parameter phi for the GLM family."
         pass
 
     @abstractmethod
-    def get_b(self, theta):
+    def _get_b(self, theta: np.ndarray) -> np.ndarray:
         "Get the cumulant function b(theta) for the GLM family."
         pass
 
     # @abstractmethod
-    # def get_c(self, y, phi):
+    # def _get_c(self, y, phi):
     #
     #    "Get the function c(y, phi) for the GLM family."
     #    pass
 
     @abstractmethod
-    def get_mu(self, theta):
+    def _get_mu(self, theta: np.ndarray) -> np.ndarray:
         "Get the mean mu(theta) for the GLM family."
         pass
 
     @abstractmethod
-    def get_link(self, mu):
+    def _get_link(self, mu: np.ndarray) -> np.ndarray:
         "Get the link function theta(mu) for the GLM family."
         pass
 
     @abstractmethod
-    def update_detadmu(self, mu):
+    def _update_detadmu(self, mu: np.ndarray) -> np.ndarray:
         "Get the derivative of mu(theta) with respect to theta for the GLM family."
         pass
 
     @abstractmethod
-    def get_theta(self, mu):
+    def _get_theta(self, mu: np.ndarray) -> np.ndarray:
         "Get the mechanical link theta(mu) for the GLM family."
         pass
 
     @abstractmethod
-    def get_V(self, mu):
+    def _get_V(self, mu: np.ndarray) -> np.ndarray:
         "Get the variance function V(mu) for the GLM family."
         pass
 
-    def update_v(self, y, mu, detadmu):
+    def _update_v(
+        self, y: np.ndarray, mu: np.ndarray, detadmu: np.ndarray
+    ) -> np.ndarray:
         "Get (running) dependent variable v for the GLM family."
         return (y - mu) * detadmu
 
-    def update_W(self, mu):
+    def _update_W(self, mu: np.ndarray) -> np.ndarray:
         "Get (running) weights W for the GLM family."
-        return 1 / (self.update_detadmu(mu=mu) ** 2 * self.get_V(mu=mu))
+        return 1 / (self._update_detadmu(mu=mu) ** 2 * self._get_V(mu=mu))
         # return (mu * (1 - mu)).reshape(-1, 1)
 
-    def update_W_tilde(self, W):
+    def _update_W_tilde(self, W: np.ndarray) -> np.ndarray:
         "Get W_tilde (formula 3.2)."
         return np.sqrt(W)
 
-    def update_v_tilde(self, y, mu, W_tilde, detadmu):
+    def _update_v_tilde(
+        self, y: np.ndarray, mu: np.ndarray, W_tilde: np.ndarray, detadmu: np.ndarray
+    ) -> np.ndarray:
         "Get v_tilde (formula 3.2)."
         return W_tilde * ((y - mu) * detadmu)
 
-    def update_X_tilde(self, W_tilde, X):
+    def _update_X_tilde(self, W_tilde: np.ndarray, X: np.ndarray) -> np.ndarray:
         "Get X_tilde (formula 3.2)."
         # import pdb; pdb.set_trace()
         return W_tilde.reshape(-1, 1) * X
 
-    def update_beta_diff(self, X_dotdot, v_dotdot):
-        "Get the beta update difference (formula 3.5) via WLS fit."
+    def _update_beta_diff(
+        self, X_dotdot: np.ndarray, v_dotdot: np.ndarray
+    ) -> np.ndarray:
+        "Get the beta _update difference (formula 3.5) via WLS fit."
         beta_diff = np.linalg.lstsq(X_dotdot, v_dotdot.reshape(-1, 1), rcond=None)[
             0
         ].flatten()
         return beta_diff
 
-    def update_eta(self, W_tilde, v_tilde, v_dotdot, X_dotdot, beta_diff, eta):
-        "Get the eta update (formula 4.5)."
+    def _update_eta(
+        self,
+        W_tilde: np.ndarray,
+        v_tilde: np.ndarray,
+        v_dotdot: np.ndarray,
+        X_dotdot: np.ndarray,
+        beta_diff: np.ndarray,
+        eta: np.ndarray,
+    ) -> np.ndarray:
+        "Get the eta _update (formula 4.5)."
         # import pdb; pdb.set_trace()
         # return  (v_tilde  - X_dotdot @ beta_diff) / W_tilde +
         return eta + X_dotdot @ beta_diff / W_tilde
         # return  (v_tilde - v_dotdot - X_dotdot @ beta_diff) / W_tilde + eta
 
-    def get_gradient(self, Z, W, v):
+    def _get_gradient(self, Z: np.ndarray, W: np.ndarray, v: np.ndarray) -> np.ndarray:
         return Z.T @ W @ v
 
-    def compute_diff(self, deviance, last):
+    def _get_diff(self, deviance: np.ndarray, last: np.ndarray) -> np.ndarray:
         return np.abs(deviance - last) / (0.1 + np.abs(last))
 
-    def residualize(self, v, X, flist, weights, tol):
+    def residualize(
+        self,
+        v: np.ndarray,
+        X: np.ndarray,
+        flist: np.ndarray,
+        weights: np.ndarray,
+        tol: np.ndarray,
+    ) -> tuple[np.ndarray, np.ndarray]:
         "Residualize v and X by flist using weights."
         if flist is None:
             return v, X
@@ -259,9 +287,9 @@ class Feglm(Fepois, ABC):
             else:
                 return vX_resid[:, 0], vX_resid[:, 1:]
 
-    def stop_iterating(self, crit, tol, r, maxiter):
-        stop_iterating = crit < tol
-        converged = stop_iterating
+    def _stop_iterating(self, crit: float, tol: float, r: int, maxiter: int) -> bool:
+        _stop_iterating = crit < tol
+        converged = _stop_iterating
         if r == maxiter:
             raise NonConvergenceError(
                 f"""
@@ -272,28 +300,28 @@ class Feglm(Fepois, ABC):
 
         return converged
 
-    def update_eta_step_halfing(
+    def _update_eta_step_halfing(
         self,
-        Y,
-        beta,
-        eta,
-        mu,
-        deviance,
-        beta_update_diff,
-        W_tilde,
-        v_tilde,
-        v_dotdot,
-        X_dotdot,
-        deviance_old,
-        step_halfing_tolerance,
-    ):
+        Y: np.ndarray,
+        beta: np.ndarray,
+        eta: np.ndarray,
+        mu: np.ndarray,
+        deviance: np.ndarray,
+        beta_update_diff: np.ndarray,
+        W_tilde: np.ndarray,
+        v_tilde: np.ndarray,
+        v_dotdot: np.ndarray,
+        X_dotdot: np.ndarray,
+        deviance_old: np.ndarray,
+        step_halfing_tolerance: float,
+    ) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, bool]:
         "Update parameters, potentially using step halfing."
         alpha = 1.0
         step_accepted = False
 
         while alpha > step_halfing_tolerance:
             beta_try = beta + alpha * beta_update_diff
-            eta_try = self.update_eta(
+            eta_try = self._update_eta(
                 W_tilde=W_tilde.flatten(),
                 v_tilde=v_tilde,
                 v_dotdot=v_dotdot,
@@ -301,15 +329,15 @@ class Feglm(Fepois, ABC):
                 beta_diff=alpha * beta_update_diff,
                 eta=eta,
             )
-            mu_try = self.get_mu(theta=eta_try)
-            deviance_try = self.compute_deviance(Y.flatten(), mu_try)
+            mu_try = self._get_mu(theta=eta_try)
+            deviance_try = self._get_deviance(Y.flatten(), mu_try)
             if deviance_try < deviance_old:
                 beta = beta_try
                 eta = eta_try
                 mu = mu_try
                 deviance = deviance_try
                 step_accepted = True
-                crit = self.compute_diff(deviance, deviance_old)
+                crit = self._get_diff(deviance, deviance_old)
                 break
             else:
                 alpha /= 2.0
@@ -321,6 +349,8 @@ class Feglm(Fepois, ABC):
 
 
 class Felogit(Feglm):
+    "Class for the estimation of a fixed-effects logit model."
+
     def __init__(
         self,
         FixestFormula: FixestFormula,
@@ -367,34 +397,36 @@ class Felogit(Feglm):
 
         self._method = "feglm-logit"
 
-    def compute_deviance(self, y, mu):
+    def _get_deviance(self, y: np.ndarray, mu: np.ndarray) -> np.ndarray:
         return -2 * np.sum(y * np.log(mu) + (1 - y) * np.log(1 - mu))
 
-    def get_dispersion_phi(self, theta):
+    def _get_dispersion_phi(self, theta: np.ndarray) -> int:
         return 1
 
-    def get_b(self, theta):
+    def _get_b(self, theta: np.ndarray) -> np.ndarray:
         return np.log(1 + np.exp(theta))
 
-    # def get_c(self, y, phi):
+    # def _get_c(self, y, phi):
     #    return 0
-    def get_mu(self, theta):
+    def _get_mu(self, theta: np.ndarray) -> np.ndarray:
         return np.exp(theta) / (1 + np.exp(theta))
 
-    def get_link(self, mu):
+    def _get_link(self, mu: np.ndarray) -> np.ndarray:
         return np.log(mu / (1 - mu))
 
-    def update_detadmu(self, mu):
+    def _update_detadmu(self, mu: np.ndarray) -> np.ndarray:
         return 1 / (mu * (1 - mu))
 
-    def get_theta(self, mu):
+    def _get_theta(self, mu: np.ndarray) -> np.ndarray:
         return np.log(mu / (1 - mu))
 
-    def get_V(self, mu):
+    def _get_V(self, mu: np.ndarray) -> np.ndarray:
         return mu * (1 - mu)
 
 
 class Feprobit(Feglm):
+    "Class for the estimation of a fixed-effects probit model."
+
     def __init__(
         self,
         FixestFormula: FixestFormula,
@@ -441,29 +473,29 @@ class Feprobit(Feglm):
 
         self._method = "feglm-probit"
 
-    def compute_deviance(self, y, mu):
+    def _get_deviance(self, y: np.ndarray, mu: np.ndarray) -> np.ndarray:
         return -2 * np.sum(
             y * np.log(norm.cdf(mu)) + (1 - y) * np.log(1 - norm.cdf(mu))
         )
 
-    def get_dispersion_phi(self, theta):
+    def _get_dispersion_phi(self, theta: np.ndarray) -> int:
         return 1
 
-    def get_b(self, theta):
-        raise ValueError("The function get_b is not implemented for the probit model.")
+    def _get_b(self, theta: np.ndarray) -> np.ndarray:
+        raise ValueError("The function _get_b is not implemented for the probit model.")
         return None
 
-    def get_mu(self, theta):
+    def _get_mu(self, theta: np.ndarray) -> np.ndarray:
         return norm.cdf(theta)
 
-    def get_link(self, mu):
+    def _get_link(self, mu: np.ndarray) -> np.ndarray:
         return norm.ppf(mu)
 
-    def update_detadmu(self, mu):
+    def _update_detadmu(self, mu: np.ndarray) -> np.ndarray:
         return 1 / norm.pdf(norm.ppf(mu))
 
-    def get_theta(self, mu):
+    def _get_theta(self, mu: np.ndarray) -> np.ndarray:
         return norm.ppf(mu)
 
-    def get_V(self, mu):
+    def _get_V(self, mu: np.ndarray) -> np.ndarray:
         return mu * (1 - mu)
