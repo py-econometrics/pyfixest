04cd53864dde87a8d66f096285369a96f7a4e679
Alexander Fischer <alexander-fischer1801@t-online.de>
2025-02-16 13:56:19 +0100

    Add a `rename_models` arg to plotting methods (#813)
    * add rename_model arg to coefplot, iplot
    
    * add tests
    
    * update changelog
    
    * more tests
    
    * fix documentation error
    
    * add docs error
    
    * update changelog
    
    * tweaks
    
    * no need for list conversion

--- pyfixest/report/summarize.py
+++ pyfixest/report/summarize.py
@@ -585,7 +585,9 @@ def summary(models: ModelInputType, digits: int = 3) -> None:
 
 
 def _post_processing_input_checks(
-    models: ModelInputType, check_duplicate_model_names: bool = False
+    models: ModelInputType,
+    check_duplicate_model_names: bool = False,
+    rename_models: Optional[dict[str, str]] = None,
 ) -> list[Union[Feols, Fepois, Feiv]]:
     """
     Perform input checks for post-processing models.
@@ -599,6 +601,9 @@ def _post_processing_input_checks(
                 Whether to check for duplicate model names. Default is False.
                 Mostly used to avoid overlapping models in plots created via
                 pf.coefplot() and pf.iplot().
+        rename_models : dict, optional
+                A dictionary to rename the models. The keys are the original model names
+                and the values are the new model names.
 
     Returns
     -------
@@ -627,10 +632,12 @@ def _post_processing_input_checks(
     else:
         raise TypeError("Invalid type for models argument.")
 
+    if check_duplicate_model_names or rename_models is not None:
+        all_model_names = [model._model_name for model in models_list]
+
     if check_duplicate_model_names:
         # create model_name_plot attribute to differentiate between models with the
         # same model_name / model formula
-        all_model_names = [model._model_name for model in models_list]
         for model in models_list:
             model._model_name_plot = model._model_name
 
@@ -645,6 +652,16 @@ def _post_processing_input_checks(
                 model._model_name_plot = f"Model {i}: {model._model_name}"
                 warnings.warn(
                     f"The _model_name attribute {model._model_name}' is duplicated for models in the `models` you provided. To avoid overlapping model names / plots, the _model_name_plot attribute has been changed to '{model._model_name_plot}'."
+                )
+
+        if rename_models is not None:
+            model_name_diff = set(rename_models.keys()) - set(all_model_names)
+            if model_name_diff:
+                warnings.warn(
+                    f"""
+                    The following model names specified in rename_models are not found in the models:
+                    {model_name_diff}
+                    """
                 )
 
     return models_list
