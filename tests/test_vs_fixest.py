80227296d9c0696b3a5e9864c0e873ba8fafaa65
AronNemeth <aron.nemeth8@gmail.com>
2025-02-27 12:49:37 +0100

    add tests for adj R2 values

--- tests/test_vs_fixest.py
+++ tests/test_vs_fixest.py
@@ -331,13 +331,17 @@ def test_single_fit_feols(
     py_adj_r2_within = mod._adj_r2_within
     r_r = fixest.r2(r_fixest)
     r_r2 = r_r[1]
+    r_adj_r2 = r_r[2]
     r_r2_within = r_r[5]
+    r_adj_r2_within = r_r[6]
 
     check_absolute_diff(py_r2, r_r2, 1e-08, "py_r2 != r_r2")
+    check_absolute_diff(py_adj_r2, r_adj_r2, 1e-08, "py_adj_r2 != r_adj_r2")
     if not np.isnan(py_r2_within):
         check_absolute_diff(
             py_r2_within, r_r2_within, 1e-08, "py_r2_within != r_r2_within"
         )
+        check_absolute_diff(py_adj_r2_within, r_adj_r2_within, 1e-08, "py_adj_r2_within != r_adj_r2_within")
 
 
 @pytest.mark.slow
