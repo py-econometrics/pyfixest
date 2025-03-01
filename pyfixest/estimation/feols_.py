Fixed adjusted R2 value calculation

pyfixest/estimation/feols_.py
@@ -1872,7 +1872,12 @@ class Feols:
         _has_fixef = self._has_fixef
         _weights = self._weights
 
-        _adj_factor = (_N - _has_intercept) / (_N - _k)
+        if _has_fixef:
+            _k_fe = np.sum(self._k_fe - 1) + 1
+            _adj_factor = (_N - _has_intercept) / (_N - _k - _k_fe)
+            _adj_factor_within = (_N - _k_fe) / (_N - _k - _k_fe)
+        else:
+            _adj_factor = (_N - _has_intercept) / (_N - _k)
 
         ssu = np.sum(_u_hat**2)
         ssy = np.sum(_weights * (_Y - np.average(_Y, weights=_weights)) ** 2)
@@ -1881,11 +1886,9 @@ class Feols:
         self._adj_r2 = 1 - (ssu / ssy) * _adj_factor
 
         if _has_fixef:
-            _k_fe = np.sum(self._k_fe - 1) + 1
-            _adj_factor_fe = (_N - _k_fe) / (_N - _k - _k_fe)
             ssy_within = np.sum(_Y_within**2)
             self._r2_within = 1 - (ssu / ssy_within)
-            self._r2_adj_within = 1 - (ssu / ssy_within) * _adj_factor_fe
+            self._adj_r2_within = 1 - (ssu / ssy_within) * _adj_factor_within
 
     def tidy(
         self,