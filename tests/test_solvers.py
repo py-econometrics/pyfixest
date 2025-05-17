8ab554c2eb1e1aacd0d5dabf5adf002393ebd120
AronNemeth <aron.nemeth8@gmail.com>
2025-05-17 08:31:30 +0200

    Update tests_solvers: new test matrix is symmetric and positive definite -> Cholesky works

--- tests/test_solvers.py
+++ tests/test_solvers.py
@@ -6,11 +6,11 @@ from pyfixest.estimation.solvers import solve_ols
 
 def test_solve_ols_simple_2x2():
     # Test case 1: Simple 2x2 system
-    tZX = np.array([[1, 2], [3, 4]])
-    tZY = np.array([5, 6])
-    solver = "np.linalg.lstsq"
+    tZX = np.array([[4, 2], [2, 3]])
+    tZY = np.array([10, 8])
+    solver = "scipy.linalg.solve"
     solution = solve_ols(tZX, tZY, solver)
-    assert np.allclose(solution, np.array([-4.0, 4.5]))
+    assert np.allclose(solution, np.array([1.75, 1.5]))
     # Verify solution satisfies the system
     assert np.allclose(tZX @ solution, tZY)
 
@@ -19,21 +19,33 @@ def test_solve_ols_identity():
     # Test case 2: Identity matrix
     tZX = np.eye(2)
     tZY = np.array([1, 2])
-    solver = "np.linalg.lstsq"
+    solver = "scipy.linalg.solve"
     assert np.allclose(solve_ols(tZX, tZY, solver), tZY)
 
 
 @pytest.mark.parametrize(
     argnames="solver",
-    argvalues=["scipy.linalg.solve","np.linalg.lstsq", "np.linalg.solve", "scipy.sparse.linalg.lsqr", "jax"],
-    ids=["scipy.linalg.solve","np.linalg.lstsq", "np.linalg.solve", "scipy.sparse.linalg.lsqr", "jax"],
+    argvalues=[
+        "scipy.linalg.solve",
+        "np.linalg.lstsq",
+        "np.linalg.solve",
+        "scipy.sparse.linalg.lsqr",
+        "jax",
+    ],
+    ids=[
+        "scipy.linalg.solve",
+        "np.linalg.lstsq",
+        "np.linalg.solve",
+        "scipy.sparse.linalg.lsqr",
+        "jax",
+    ],
 )
 def test_solve_ols_different_solvers(solver):
     # Test case 3: Test different solvers give same result
-    tZX = np.array([[1, 2], [3, 4]])
-    tZY = np.array([5, 6])
+    tZX = np.array([[4, 2], [2, 3]])
+    tZY = np.array([10, 8])
     solution = solve_ols(tZX, tZY, solver)
-    assert np.allclose(solution, np.array([-4.0, 4.5]))
+    assert np.allclose(solution, np.array([1.75, 1.5]))
     # Verify solution satisfies the system
     assert np.allclose(tZX @ solution, tZY)
 
