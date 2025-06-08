25a2c1e26f9e9d2d35590e15c2fe2ac12b878b55
Alexander Fischer <alexander-fischer1801@t-online.de>
2025-06-08 21:31:51 +0200

    implement tests against Stata::qreg2 for crv errors

--- tests/test_quantreg.py
+++ tests/test_quantreg.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import pytest
 import rpy2.robjects as ro
 from rpy2.robjects import pandas2ri
@@ -14,6 +15,41 @@ stats = importr("stats")
 pandas2ri.activate()
 
 
+@pytest.fixture
+def stata_results_crv():
+    """
+    Results from Stata's qreg2 package.
+    For code on how the Stata results were generated, see
+    this issue: https://github.com/py-econometrics/pyfixest/issues/923.
+    """
+    return pd.DataFrame(
+        {
+            "fml": ["Y~X1", "Y~X1", "Y~X1", "Y~X1+X2", "Y~X1+X2", "Y~X1+X2"],
+            "quantile": [0.35, 0.50, 0.95, 0.35, 0.50, 0.95],
+            "Intercept": [
+                -0.1282921,
+                0.919176,
+                6.036114,
+                0.3199207,
+                1.071671,
+                4.030318,
+            ],
+            "coef_X1": [1.692162, 1.730726, 1.695720, 1.653593, 1.592271, 1.664043],
+            "coef_X2": [np.nan, np.nan, np.nan, 0.8902029, 0.8690366, 0.8852484],
+            "se_Intercept": [
+                0.1939684,
+                0.216606,
+                0.3964476,
+                0.189286,
+                0.1869863,
+                0.2984174,
+            ],
+            "se_X1": [0.0992838, 0.1270346, 0.1482422, 0.0347237, 0.0486158, 0.1144752],
+            "se_X2": [np.nan, np.nan, np.nan, 0.0210688, 0.0274879, 0.0278543],
+        }
+    )
+
+
 @pytest.mark.parametrize(
     "fml",
     [
@@ -24,7 +60,7 @@ pandas2ri.activate()
 @pytest.mark.parametrize(
     "vcov",
     [
-    #    "iid",
+        #    "iid",
         "nid",
     ],
 )
@@ -62,3 +98,30 @@ def test_qplot():
     fit2 = pf.quantreg("Y ~ X1 + X2", data=data, quantile=0.9, method="fn")
 
     pf.qplot([fit1, fit2])
+
+
+@pytest.mark.parametrize("fml", ["Y~X1", "Y~X1+X2"])
+@pytest.mark.parametrize("data", [pf.get_data(seed=12).dropna()])
+@pytest.mark.parametrize("quantile", [0.35, 0.5, 0.95])
+def test_quantreg_crv(data, fml, quantile, stata_results_crv):
+    "Test quantreg's CRV errors vs Stata's qreg2."
+
+    def expected(q, cols):
+        row = stata_results_crv[
+            (stata_results_crv["fml"] == fml) & (stata_results_crv["quantile"] == q)
+        ]
+        return row[cols].to_numpy().ravel()
+
+    fit = pf.quantreg(fml, data=data, vcov={"CRV1": "f1"}, quantile=quantile)
+
+    coef = fit.coef().to_numpy()
+    se = fit.se().to_numpy()
+
+    coef_cols = ["Intercept", "coef_X1"] + (["coef_X2"] if "X2" in fml else [])
+    se_cols = ["se_Intercept", "se_X1"] + (["se_X2"] if "X2" in fml else [])
+
+    exp_coef = expected(quantile, coef_cols)
+    exp_se = expected(quantile, se_cols)
+
+    np.testing.assert_allclose(coef, exp_coef, rtol=2e-3, atol=2e-3)
+    np.testing.assert_allclose(se, exp_se, rtol=2e-3, atol=2e-3)
