66fc40fe75f3e62a9a6cb94edd74ea54e9fbba61
Alexander Fischer <alexander-fischer1801@t-online.de>
2025-06-12 22:19:29 +0200

    add third party permission statement

--- src/collinear.rs
+++ src/collinear.rs
@@ -11,6 +11,11 @@
 // originally licensed under GPL-3.0.  Laurent Berge granted the maintainers of this
 // project an irrevocable, written permission to
 // redistribute and re-license the relevant code under the MIT License.
+// The full text of that permission is archived at:
+//
+// docs/THIRD_PARTY_PERMISSIONS.md
+//
+// ----------------------------------------------------------------------
 
 use ndarray::{Array1, Array2, ArrayView2};
 use numpy::IntoPyArray;
