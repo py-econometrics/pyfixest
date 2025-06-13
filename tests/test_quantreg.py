0f3e26595fea9be2ab16460ee65d7ea1605d989f
Alexander Fischer <alexander-fischer1801@t-online.de>
2025-06-13 22:22:00 +0200

    adjust tests

--- tests/test_quantreg.py
+++ tests/test_quantreg.py
@@ -60,36 +60,44 @@ def stata_results_crv():
 @pytest.mark.parametrize(
     "vcov",
     [
-        #    "iid",
         "nid",
     ],
 )
-@pytest.mark.parametrize("data", [pf.get_data()])
-@pytest.mark.parametrize("quantile", [0.35, 0.5, 0.9])
+@pytest.mark.parametrize("data", [pf.get_data(N=5_000, seed=3131)])
+@pytest.mark.parametrize("quantile", [0.02, 0.35, 0.5, 0.9])
 @pytest.mark.parametrize("method", ["fn"])
 def test_quantreg_vs_r(data, fml, vcov, quantile, method):
     "Test that pyfixest's quantreg implementation equals R's quantreg implementation."
     # Fit model in pyfixest
-    fit_py = pf.quantreg(fml, data=data, vcov=vcov, quantile=quantile, method=method)
+
+    rng = np.random.default_rng(3993)
+    data["Y"] = 1 + 2 * data["X1"] + rng.normal(size=len(data))
+    data["Y"] = data["Y"] + 3 * data["X2"] if "X2" in fml else data["Y"]
+
+    tol = 1e-6
+
+    fit_py = pf.quantreg(
+        fml, data=data, vcov=vcov, quantile=quantile, method=method, tol=tol
+    )
 
     # Fit model in R
     r_data = pandas2ri.py2rpy(data)
     r_formula = ro.Formula(fml)
 
     # Fit R model
-    fit_r = quantreg.rq(r_formula, data=r_data, tau=quantile, method=method)
+    fit_r = quantreg.rq(r_formula, data=r_data, tau=quantile, method=method, eps=tol)
 
     # Compare coefficients
     py_coef = fit_py.coef().to_numpy()
     r_coef = np.array(fit_r.rx2("coefficients"))
-    np.testing.assert_allclose(py_coef, r_coef, rtol=2e-03, atol=2e-03)
+    np.testing.assert_allclose(py_coef, r_coef, rtol=1e-03, atol=1e-08)
 
     # compare standard errors
     py_se = fit_py.se().to_numpy()
     r_summ = ro.r["summary"](fit_r, se=vcov)
     coeff_mat = r_summ.rx2("coefficients")
     r_se = np.array(coeff_mat)[:, 1]
-    np.testing.assert_allclose(py_se, r_se, rtol=2e-03, atol=2e-03)
+    np.testing.assert_allclose(py_se, r_se, rtol=1e-03, atol=1e-08)
 
 
 def test_qplot():
