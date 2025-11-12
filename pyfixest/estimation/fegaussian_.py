7288fe048463dbe4ffc351e62d6dba6fa0993839
Alexander Fischer <alexander-fischer1801@t-online.de>
2025-11-12 20:05:21 +0100

    only bread for iid inference

--- pyfixest/estimation/fegaussian_.py
+++ pyfixest/estimation/fegaussian_.py
@@ -94,15 +94,6 @@ class Fegaussian(Feglm):
     def _get_V(self, mu: np.ndarray) -> np.ndarray:
         return np.ones_like(mu)
 
-    def _vcov_iid(self):
-        _N = self._N
-        _u_hat = self._u_hat
-        _bread = self._bread
-        sigma2 = np.sum(_u_hat.flatten() ** 2) / (_N)
-        _vcov = _bread * sigma2
-
-        return _vcov
-
     def _get_score(
         self, y: np.ndarray, X: np.ndarray, mu: np.ndarray, eta: np.ndarray
     ) -> np.ndarray:
--- tests/test_feols_feglm_internally.py
+++ tests/test_feols_feglm_internally.py
@@ -49,12 +49,9 @@ def test_ols_vs_gaussian_glm(fml, inference, dropna):
     check_absolute_diff(fit_ols._u_hat[0:5], fit_gaussian._u_hat[0:5], tol=1e-10)
     check_absolute_diff(fit_ols._scores[0, :], fit_gaussian._scores[0, :], tol=1e-10)
 
-    if inference != "iid":
-        # iid inference different: follows iid-glm; just the bread and not bread x sigma2
-        check_absolute_diff(
-            fit_ols.se().xs("X1"), fit_gaussian.se().xs("X1"), tol=1e-10
-        )
-        check_absolute_diff(fit_ols._vcov[0, 0], fit_gaussian._vcov[0, 0], tol=1e-10)
+    # iid inference different: follows iid-glm; just the bread and not bread x sigma2
+    check_absolute_diff(fit_ols.se().xs("X1"), fit_gaussian.se().xs("X1"), tol=1e-10)
+    check_absolute_diff(fit_ols._vcov[0, 0], fit_gaussian._vcov[0, 0], tol=1e-10)
 
 
 @pytest.mark.skip("Fixed effects are not yet supported.")
