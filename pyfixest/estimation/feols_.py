44d872578d432d02ec1348ad1848c20972286cf3
Alexander Fischer <alexander-fischer1801@t-online.de>
2026-06-11 21:41:33 +0200

    Remove root estimation shims

--- pyfixest/estimation/feols_.py
+++
@@ -1,8 +0,0 @@
-"""Backward-compatible shim. Real module at pyfixest.estimation.models.feols_."""
-
-from pyfixest.estimation.models.feols_ import *  # noqa: F403
-from pyfixest.estimation.models.feols_ import (  # noqa: F401
-    Feols,
-    _check_vcov_input,
-    _deparse_vcov_input,
-)
